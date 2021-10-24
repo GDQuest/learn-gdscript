edcc5a19c103c897868ab6619e975610aad22878
Nathan Lovato <nathan@gdquest.com>
2021-10-24 09:56:36 -0600

    feat: animate revealers
    fix: correct revealer height calculation
    fix: fix revealer being offset horizontally
    fix: fix revealer being too wide

--- addons/exporter-for-live-editor/ui/Revealer.gd
+++ addons/exporter-for-live-editor/ui/Revealer.gd
@@ -2,6 +2,9 @@ tool
 class_name Revealer, "./Revealer.svg"
 extends Container
 
+# Duration of the tween animation in seconds.
+const ANIM_DURATION := 0.1
+
 export var title := "Expand" setget set_title
 export var is_expanded := false setget set_is_expanded
 export var revealer_height := 32.0 setget set_revealer_height
@@ -21,12 +24,12 @@ onready var _tween: Tween = $Tween
 func _ready() -> void:
 	_button.pressed = is_expanded
 	_button.connect("toggled", self, "set_is_expanded")
-	update_min_size()
 	for child in get_children():
 		if child == _button or not child is Control:
 			continue
 		_contents.append(child)
-	# Through the constructor setter call, the _contents variable will be empty, so we need to call this again. 
+	# Through the constructor setter call, the _contents variable will be empty,
+	# so we need to call this again.
 	set_is_expanded(is_expanded)
 
 
@@ -42,9 +45,9 @@ func set_is_expanded(new_is_expanded: bool) -> void:
 		yield(self, "ready")
 
 	if is_expanded:
-		_rotate_chevron(90, _height)
+		_rotate_chevron(90.0)
 	else:
-		_rotate_chevron(0, _button.rect_size.y)
+		_rotate_chevron(0.0)
 
 	for node in _contents:
 		node.visible = is_expanded
@@ -61,48 +64,45 @@ func set_revealer_height(new_revealer_height: float) -> void:
 	revealer_height = new_revealer_height
 	if not is_inside_tree():
 		yield(self, "ready")
-	rect_min_size.y = revealer_height
-	_button.rect_size.y = revealer_height
+	update_min_size()
 
 
 func update_min_size() -> void:
 	rect_min_size.x = max(_button.rect_size.x, rect_size.x)
-	rect_min_size.y = revealer_height
-	_button.rect_size.y = revealer_height
-	if is_expanded:
-		for node in _contents:
-			rect_min_size.y += node.rect_min_size.y
+	_button.rect_size.x = rect_min_size.x
+	# rect_min_size.y = _height
+	if _tween.is_inside_tree():
+		_tween.stop(self, "rect_min_size:y")
+		_tween.interpolate_property(self, "rect_min_size:y", rect_min_size.y, _height, ANIM_DURATION)
 
 
 func sort_children() -> void:
-	var top := 0.0
-	update_min_size()
-	_height = _button.rect_size.y
-	for index in get_child_count():
-		var child: Node = get_child(index)
-		if not child is Control or not child.visible:
+	var top := _button.rect_size.y
+	var index := 0
+	for node in _contents:
+		if not node.visible:
 			continue
-		var node_padding := 0.0 if index == 0 else padding
-		var margin := 0.0 if index == 0 else first_margin if index == 1 else children_margin
-		top = _fit_child(child, top + margin, node_padding)
+		var margin := first_margin if index == 0 else children_margin
+		top = _fit_child(node, top + margin, padding)
+		index += 1
 	_height = top
+	update_min_size()
 
 
-func _rotate_chevron(to: float, height: float, time := .2) -> void:
+func _rotate_chevron(rotation_degrees: float, time := ANIM_DURATION) -> void:
 	if not _tween.is_inside_tree():
-		_chevron.rect_rotation = to
-		rect_size.y = height
+		_chevron.rect_rotation = rotation_degrees
 		return
 	_tween.stop_all()
-	_tween.interpolate_property(_chevron, "rect_rotation", _chevron.rect_rotation, to, time)
-	_tween.interpolate_property(self, "rect_min_size:y", rect_min_size.y, to, time)
+	_tween.interpolate_property(_chevron, "rect_rotation", _chevron.rect_rotation, rotation_degrees, time)
 	_tween.start()
 
 
 func _fit_child(control: Control, top := 0.0, node_padding := 0.0) -> float:
 	var position := Vector2(node_padding, top)
-	var height := control.rect_size.y
+	var height := control.rect_min_size.y
 	var size := Vector2(rect_size.x - node_padding, height)
 	var rect := Rect2(position, size)
 	fit_child_in_rect(control, rect)
+	control.rect_size.x = size.x
 	return top + height
