7540ccd491219534cb3fb376d61836dc1f82fb3a
Yuri Sizov <yuris@humnom.net>
2021-12-17 18:47:37 +0300

    fix: update report form contents to properly direct users

--- ui/components/ReportFormPopup.gd
+++ ui/components/ReportFormPopup.gd
@@ -1,7 +1,14 @@
 extends ColorRect
 
 onready var _confirm_button := $PanelContainer/Column/Margin/Column/ConfirmButton as Button
+onready var _summary_label := $PanelContainer/Column/Margin/Column/Summary as RichTextLabel
 
 
 func _ready():
 	_confirm_button.connect("pressed", self, "hide")
+	_summary_label.connect("meta_clicked", self, "_on_meta_clicked")
+
+
+func _on_meta_clicked(data) -> void:
+	if typeof(data) == TYPE_STRING and data.begins_with("https://"):
+		OS.shell_open(data)
