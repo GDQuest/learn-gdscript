305c351e99e612e348a7ce8deefeeb17aaa6c2d6
johnnygossdev <me@johnnygoss.dev>
2021-12-20 17:48:39 +0000

    content: add tests for practices

--- course/lesson-4-drawing-a-rectangle/TestsDrawingBiggerRectangle.gd
+++ course/lesson-4-drawing-a-rectangle/TestsDrawingBiggerRectangle.gd
@@ -1,13 +1,13 @@
 extends PracticeTester
 
-var target_polygon := [Vector2(0, 0), Vector2(200, 0), Vector2(200, 120), Vector2(0, 200), Vector2(0, 0)]
+var target_polygon := [Vector2(0, 0), Vector2(220, 0), Vector2(220, 260), Vector2(0, 260), Vector2(0, 0)]
 
 
 func _init() -> void:
 	target_polygon.sort()
 
 
-func test_rectangle() -> String:
+func test_bigger_rectangle() -> String:
 	var turtle: DrawingTurtle = _scene_root_viewport.get_child(0)
 	var polygons := turtle.get_polygons()
 	if polygons.empty():
@@ -16,6 +16,6 @@ func test_rectangle() -> String:
 	var square: DrawingTurtle.Polygon = polygons[0]
 	square.points.sort()
 	if square.points != target_polygon:
-		return "The drawn shape is not a rectangle with a width of 200 and a length of 120."
+		return "The drawn shape is not a rectangle with a width of 220 and a length of 260."
 
 	return ""
