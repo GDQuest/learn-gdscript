b97ce11c9eaf86a74f919d6b5b2abe49056c33fa
Nathan Lovato <nathan@gdquest.com>
2021-12-20 15:36:36 -0600

    refactor: move, rename code, and use coroutine in DrawingTurtle

--- game_demos/DrawingTurtle.gd
+++ game_demos/DrawingTurtle.gd
@@ -22,11 +22,14 @@ var draw_speed := 400.0
 
 var _points := []
 var _polygons := []
-var _current_polygon_index := 0
 var _current_offset := Vector2.ZERO
+var _current_polygon: Polygon
+# If false, stop playing an active animation.
+var _is_playing := false
 
 onready var _sprite = $Sprite as Sprite
 onready var _canvas = $Canvas as Node2D
+
 
 # Virtually moves the turtle and records a new vertex.
 func move_forward(distance: float) -> void:
@@ -61,6 +64,7 @@ func jump(x: float, y: float) -> void:
 # Resets the turtle's state. Use it when testing a student's assignments to
 # reset the object between runs.
 func reset() -> void:
+	_is_playing = false
 	rotation_degrees = 0
 	_points.clear()
 	_polygons.clear()
@@ -68,36 +72,43 @@ func reset() -> void:
 	_current_offset = Vector2.ZERO
 
 
-# Starts the animation drawing every polygon inside the _polygons array.
-func play_draw_animation() -> void:
-	
-	if not _points.empty():
-		_close_polygon()
-	
-	if _current_polygon_index >= _polygons.size():
-		return
-	
-	var polygon := _polygons[_current_polygon_index] as Polygon
-	
-	if polygon != null:
-		_current_polygon_index += 1
-		rotation_degrees = 0
-		polygon.connect("animation_finished", self, "_polygon_finished")
-		polygon.connect("current_point", self, "_polygon_updated")
-		_canvas.add_child(polygon)
-		polygon.start()
-
-
-func _polygon_updated(point: Vector2) -> void:
-	_sprite.position = point
-
-
-func _polygon_finished() -> void:
-	play_draw_animation()
-
 # Returns a copy of the polygons the turtle will draw.
 func get_polygons() -> Array:
 	return _polygons.duplicate()
+
+
+func stop_animation() -> void:
+	_is_playing = false
+	if _current_polygon:
+		_current_polygon.stop_animation()
+
+
+# Starts the animation drawing every polygon inside the _polygons array.
+func play_draw_animation() -> void:
+	_is_playing = true
+	if not _points.empty():
+		_close_polygon()
+
+	for polygon in _polygons:
+		if not _is_playing:
+			break
+
+		_current_polygon = polygon
+		polygon = polygon as Polygon
+		if not polygon:
+			continue
+
+		rotation_degrees = 0.0
+		polygon.connect("line_end_moved", _sprite, "_change_sprite_position")
+		_canvas.add_child(polygon)
+		polygon.start_draw_animation()
+		yield(polygon, "animation_finished")
+
+	_current_polygon = null
+
+
+func _change_sprite_position(new_position: Vector2) -> void:
+	_sprite.position = new_position
 
 
 func _close_polygon() -> void:
@@ -106,35 +117,35 @@ func _close_polygon() -> void:
 
 	var polygon := Polygon.new()
 	polygon.position = _current_offset
-	polygon._segment.width = LINE_THICKNESS
-	polygon._segment.default_color = DEFAULT_COLOR
+	polygon.line_2d.width = LINE_THICKNESS
+	polygon.line_2d.default_color = DEFAULT_COLOR
 	polygon.points = PoolVector2Array(_points)
 	_polygons.append(polygon)
 	_points.clear()
 
-class Polygon extends Node2D:
-	
-	const LabelScene := preload("res://game_demos/DrawingTurtleLabel.tscn")
+
+# Polygon that can animate drawing its line.
+class Polygon:
+	extends Node2D
+
+	const LabelScene := preload("DrawingTurtleLabel.tscn")
 	var points := []
 	var time := 3.0
+	var line_2d := Line2D.new()
 	var _tween := Tween.new()
-	var _segment := Line2D.new()
-	var _gradient := Gradient.new()
 	var _current_points := []
 	var _current_point_index := 0
 	var _total_distance := 0.0
-	
-	
-	signal animation_finished()
-	signal current_point(point)
-	
+
+	signal animation_finished
+	signal line_end_moved(new_coordinates)
+
 	func _init() -> void:
 		add_child(_tween)
-		add_child(_segment)
+		add_child(line_2d)
 		_tween.connect("tween_all_completed", self, "next")
-	
-	
-	func start() -> void:
+
+	func start_draw_animation() -> void:
 		var previous_point := points[0] as Vector2
 		for index in range(1, points.size()):
 			var p := points[index] as Vector2
@@ -143,43 +154,42 @@ class Polygon extends Node2D:
 			_total_distance += distance
 		_tween.stop_all()
 		next()
-	
-	
+
 	func next() -> void:
 		if points.size() - _current_point_index < 2:
-			_animation_finished()
+			emit_signal("animation_finished")
 			return
-		
+
 		var starting_point := points[_current_point_index] as Vector2
-		var destination := points[_current_point_index+1] as Vector2
+		var destination := points[_current_point_index + 1] as Vector2
 		_current_point_index += 1
 
 		var distance := starting_point.distance_to(destination)
 		var factor := distance / _total_distance
 		var timespan := time * factor
-		
+
 		var label := LabelScene.instance() as PanelContainer
 		var label_text := label.get_node("Label") as Label
 		label_text.text = String(_current_point_index)
 		label.rect_position = starting_point - label.rect_size / 2
 		add_child(label)
-		
+
 		_current_points.append(starting_point)
-		_segment.points = _current_points
-		_tween.interpolate_method(self, "_animate_last_point", starting_point, destination, timespan)
+		line_2d.points = _current_points
+		_tween.interpolate_method(
+			self, "_animate_point_position", starting_point, destination, timespan
+		)
 		_tween.start()
-	
-	
-	func _animate_last_point(last_point: Vector2) -> void:
+
+	func stop_animation() -> void:
+		_tween.stop_all()
+
+	func _animate_point_position(point: Vector2) -> void:
 		var new_points := _current_points.duplicate()
-		new_points.push_back(last_point)
-		_segment.points = new_points
-		emit_signal("current_point", last_point)
-	
-	func _animation_finished() -> void:
-		emit_signal("animation_finished")
-	
-	
+		new_points.push_back(point)
+		line_2d.points = new_points
+		emit_signal("line_end_moved", point)
+
 	# Returns the local bounds of the polygon. That is to say, it only takes the
 	# point into account in local space, but not the polygon's `position`.
 	func get_rect() -> Rect2:
