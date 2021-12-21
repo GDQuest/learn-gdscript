ffa43283b6234c9dfece1ca477ba8afeb1db3b1d
Nathan Lovato <nathan@gdquest.com>
2021-12-20 19:51:24 -0600

    chore: allow creating gdscriptcodeexample as node

--- course/common/GDScriptCodeExample.gd
+++ course/common/GDScriptCodeExample.gd
@@ -1,5 +1,16 @@
+tool
+class_name GDScriptCodeExample
 extends TextEdit
+
+export var min_size := Vector2(600, 200) setget set_min_size
 
 
 func _ready() -> void:
+	context_menu_enabled = false
+	shortcut_keys_enabled = false
 	CodeEditorEnhancer.enhance(self)
+
+
+func set_min_size(size: Vector2) -> void:
+	min_size = size
+	rect_min_size = size
