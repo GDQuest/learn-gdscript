ea10e0e98ad73257c36922409ac0a654c2e780fb
Xananax <me@xananax.com>
2021-12-24 02:18:13 +0400

    improvement: show value when slider changes

--- course/lesson-6-multiple-function-parameters/visuals/DrawAnySquare.gd
+++ course/lesson-6-multiple-function-parameters/visuals/DrawAnySquare.gd
@@ -3,3 +3,7 @@ extends RunnableCodeExample
 
 func _ready():
 	create_slider_for("side_length", 20.0, 200.0, 10.0)
+
+func _set_instance_value(value: float, property_name: String, value_label: Label) -> void:
+	._set_instance_value(value, property_name, value_label)
+	_gdscript_text_edit.text = gdscript_code.replace(property_name, "%s [=%s]"%[property_name, value])
