6af070778b27248ce83e9304e48febcb9deb842a
johnnygossdev <me@johnnygoss.dev>
2021-12-27 10:25:51 +0000

    content: add lesson 10 practice 1 draft

---
+++ course/lesson-10-the-game-loop/TestsRotating.gd
@@ -0,0 +1,11 @@
+extends PracticeTester
+
+const TOLERANCE := 0.05
+
+func test_character_is_rotating() -> String:
+	var node_2d = _scene_root_viewport.get_child(0)
+	if node_2d.rotation < -TOLERANCE:
+		return "The robot is turned counter-clockwise! Did you turn it less than 0.5 radians?"
+	elif node_2d.rotation > TOLERANCE:
+		return "The robot is turned too far clockwise! Did you turn it more than 0.5 radians?"
+	return ""
