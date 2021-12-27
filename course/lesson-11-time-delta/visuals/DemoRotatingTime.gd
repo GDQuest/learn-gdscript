76aca5bd949736465c49e4559a75134f1b2016c8
johnnygossdev <me@johnnygoss.dev>
2021-12-27 17:38:27 +0000

    content: add lesson 11 visuals

---
+++ course/lesson-11-time-delta/visuals/DemoRotatingTime.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+var _times := [0.1, 0.1, 0.2, 0.1, 0.2]
+var _index := 0
+
+onready var _timer := $Timer
+onready var _sprite := $DemoRotateSprite
+
+
+func _on_Timer_timeout():
+	_sprite.rotate(1 * _timer.wait_time)
+	_timer.start(_times[_index])
+	
+	_index = wrapi(_index + 1, 0, _times.size() - 1)
