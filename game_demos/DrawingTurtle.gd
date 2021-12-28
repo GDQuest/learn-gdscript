411c657f66206e40c71ee7e59728afbf299f05a0
Nathan Lovato <nathan@gdquest.com>
2021-12-28 14:57:35 -0600

    fix: reset turtle position in drawing examples, merge files
    Closes #4

--- game_demos/DrawingTurtle.gd
+++ game_demos/DrawingTurtle.gd
@@ -14,6 +14,8 @@ extends Node2D
 const LINE_THICKNESS := 4.0
 const DEFAULT_COLOR := Color.white
 
+var line_draw_speed := 400.0
+
 var _points := []
 var _polygons := []
 var _current_offset := Vector2.ZERO
@@ -131,7 +133,7 @@ func _close_polygon() -> void:
 	if _points.empty():
 		return
 
-	var polygon := Polygon.new()
+	var polygon := Polygon.new(line_draw_speed)
 	# We want to test shapes being drawn at the correct position using the
 	# position property. It works differently from jump() which offsets the
 	# turtle from its position.
@@ -160,10 +162,11 @@ class Polygon:
 	signal animation_finished
 	signal line_end_moved(new_coordinates)
 
-	func _init() -> void:
+	func _init(line_draw_speed := 400.0) -> void:
 		add_child(_tween)
 		add_child(line_2d)
 		_tween.connect("tween_all_completed", self, "next")
+		draw_speed = line_draw_speed
 
 	func start_draw_animation() -> void:
 		var previous_point := points[0] as Vector2
