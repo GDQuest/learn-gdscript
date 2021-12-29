51a1c9f447482533d60a4bb7ab6aa649974c6d35
Nathan Lovato <nathan@gdquest.com>
2021-12-28 19:58:04 -0600

    improvement: improve the structure of lesson 5, clarify explanations
    fix: turtle jumping to different positions on rerun of example in lesson 6

--- course/lesson-6-multiple-function-parameters/visuals/DemoDrawingSquares.gd
+++ course/lesson-6-multiple-function-parameters/visuals/DemoDrawingSquares.gd
@@ -1,21 +1,22 @@
 extends Node2D
 
-var side_length := 40.0
+var size := 40.0
 
 onready var _turtle: DrawingTurtle = $DrawingTurtle
 
 func run():
 	_turtle.reset()
-	_turtle.move_forward(side_length)
+	_turtle.position = Vector2.ZERO
+	_turtle.move_forward(size)
 	_turtle.turn_right(90)
-	_turtle.move_forward(side_length)
+	_turtle.move_forward(size)
 	_turtle.turn_right(90)
-	_turtle.move_forward(side_length)
+	_turtle.move_forward(size)
 	_turtle.turn_right(90)
-	_turtle.move_forward(side_length)
+	_turtle.move_forward(size)
 	_turtle.turn_right(90)
 
 	_turtle.play_draw_animation()
 
-	var rect: Rect2 = _turtle.get_polygons()[0].get_rect()
-	_turtle.position = rect.position - rect.size / 2.0
+	var rect: Rect2 = _turtle.get_rect()
+	_turtle.position = - rect.size / 2.0
--- course/lesson-6-multiple-function-parameters/visuals/DrawAnySquare.gd
+++ course/lesson-6-multiple-function-parameters/visuals/DrawAnySquare.gd
@@ -2,7 +2,7 @@ tool
 extends RunnableCodeExample
 
 func _ready():
-	create_slider_for("side_length", 20.0, 200.0, 10.0)
+	create_slider_for("size", 20.0, 200.0, 10.0)
 
 func _set_instance_value(value: float, property_name: String, value_label: Label) -> void:
 	._set_instance_value(value, property_name, value_label)
