f7fd2c695dae8c10e664124f9e40a32bfafdff10
Nathan Lovato <nathan@gdquest.com>
2022-01-04 09:59:05 -0600

    improvement: move practice button checkmark, make next pill more readable

--- ui/UIPracticeButton.gd
+++ ui/UIPracticeButton.gd
@@ -6,10 +6,12 @@ signal pressed
 var completed_before := false setget set_completed_before
 var is_highlighted := false setget set_is_highlighted
 
-onready var _title_label := $Margin/Row/Column/Title as Label
+onready var _title_label := $Margin/Row/Column/Row/Title as Label
+onready var _next_pill_label := $Margin/Row/Column/Row/NextPill as Label
+
 onready var _description_label := $Margin/Row/Column/Description as Label
 onready var _button := $Margin/Row/Button as Button
-onready var _completed_before_icon := $Margin/Row/CompletedBeforeIcon as TextureRect
+onready var _completed_before_icon := $Margin/Row/Column/Row/CompletedBeforeIcon as TextureRect
 
 
 func _ready() -> void:
@@ -28,8 +30,10 @@ func setup(practice: Practice) -> void:
 
 func set_completed_before(value: bool) -> void:
 	completed_before = value
-	if is_inside_tree():
-		_completed_before_icon.visible = completed_before
+	if not _completed_before_icon:
+		yield(self, "ready")
+
+	_completed_before_icon.visible = completed_before
 
 
 func set_is_highlighted(value: bool) -> void:
@@ -37,11 +41,12 @@ func set_is_highlighted(value: bool) -> void:
 
 	if not is_inside_tree():
 		yield(self, "ready")
-
+	
+	_next_pill_label.visible = is_highlighted
 	_button.add_stylebox_override("normal", preload("res://ui/theme/button_outlined_accent_normal.tres"))
 	_button.add_stylebox_override("hover", preload("res://ui/theme/button_outlined_accent_hover.tres"))
 	_button.add_stylebox_override("pressed", preload("res://ui/theme/button_outlined_accent_pressed.tres"))
-	_button.add_color_override("font_color", Color("3dff6e"))
-	_button.add_color_override("font_color_focus", Color("3dff6e"))
-	_button.add_color_override("font_color_hover", Color("c1ffd1"))
-	_button.add_color_override("font_color_pressed", Color("1fb850"))
+	_button.add_color_override("font_color", Color("26c6f7"))
+	_button.add_color_override("font_color_focus", Color("26c6f7"))
+	_button.add_color_override("font_color_hover", Color("f5fafa"))
+	_button.add_color_override("font_color_pressed", Color("1e85e0"))
