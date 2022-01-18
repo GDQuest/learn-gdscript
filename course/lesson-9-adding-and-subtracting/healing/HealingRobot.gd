6e6eae7dbe6f7eb072f29c10ed14f0a164554165
johnnygossdev <me@johnnygoss.dev>
2022-01-18 16:17:39 +0000

    content: add practice 2

--- course/lesson-9-adding-and-subtracting/healing/HealingRobot.gd
+++ course/lesson-9-adding-and-subtracting/healing/HealingRobot.gd
@@ -1,27 +1,27 @@
 extends Control
 
-var health = 20
-var _health_gained = 100
-var _max_health = 80
+var health = 50
+var _healing = 50
+var _max_health = 100
 
-onready var _robot := $HealSprite
+onready var _robot := $RobotCharacter
 
 
 func _ready() -> void:
-	_robot.health_gained = _health_gained
 	_robot.health = health
 	_robot.max_health = _max_health
 	_robot._update_health_bar()
 
 
 func _run() -> void:
-	heal(_health_gained)
+	heal(_healing)
 	_update_robot()
+	yield(get_tree().create_timer(1.0), "timeout")
+	Events.emit_signal("practice_run_completed")
 
 
 func _update_robot() -> void:
 	_robot._animation_player.play("heal")
-	_robot.health_gained = _health_gained
 	_robot.health = health
 	_robot.max_health = _max_health
 	_robot._update_health_bar()
@@ -30,6 +30,4 @@ func _update_robot() -> void:
 # EXPORT heal
 func heal(amount):
 	health += amount
-	if health > 80:
-		health = 80
 # /EXPORT heal
