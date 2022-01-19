dfa5f90ad863bac16d653f587e0dfaf316798a22
johnnygossdev <me@johnnygoss.dev>
2022-01-19 12:42:30 +0000

    improvement: traffic light demo

--- course/lesson-15-modulo/visuals/TrafficLights.gd
+++ course/lesson-15-modulo/visuals/TrafficLights.gd
@@ -3,36 +3,34 @@ extends Node2D
 
 var light_index := -1
 
-onready var index_label := $Index
-onready var lights := [
+onready var _tween := $Tween
+onready var _index_label := $Index
+onready var _lights := [
 	$Red,
 	$Orange,
 	$Green
 ]
-onready var _tween := $Tween
 
-func _ready():
+
+func _ready() -> void:
 	run()
 
 
 func run() -> void:
 	if _tween.is_active():
 		return
+	
 	light_index += 1
 	light_index %= 3
-	
 	turn_on_light(light_index)
 	
-	index_label.text = str(light_index)
+	_index_label.text = str(light_index)
 
 
 func turn_on_light(light_index) -> void:
 	
-	for light in lights:
+	for light in _lights:
 		_tween.interpolate_property(light, "modulate:a", light.modulate.a, 0, 0.2, Tween.TRANS_EXPO, Tween.EASE_OUT)
 	
-	_tween.interpolate_property(lights[light_index], "modulate:a", 0, 1, 0.2, Tween.TRANS_EXPO, Tween.EASE_OUT, 0.2)
-	
+	_tween.interpolate_property(_lights[light_index], "modulate:a", 0, 1, 0.2, Tween.TRANS_EXPO, Tween.EASE_OUT, 0.2)
 	_tween.start()
-	
-	index_label.rect_position.y = 60 * (light_index - 1) - 20
