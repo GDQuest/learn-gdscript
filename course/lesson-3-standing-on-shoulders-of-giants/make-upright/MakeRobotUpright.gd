5f1a29a6168a9231feb8a2ac9ddd38aac32ccc84
johnnygossdev <me@johnnygoss.dev>
2022-01-20 20:36:41 +0000

    improvement: lessons 1-3 pass

--- course/lesson-3-standing-on-shoulders-of-giants/make-upright/MakeRobotUpright.gd
+++ course/lesson-3-standing-on-shoulders-of-giants/make-upright/MakeRobotUpright.gd
@@ -7,3 +7,5 @@ func _run():
 	# EXPORT rotate
 	rotate(0.5)
 	# /EXPORT rotate
+	yield(get_tree().create_timer(1.0), "timeout")
+	Events.emit_signal("practice_run_completed")
--- course/lesson-3-standing-on-shoulders-of-giants/make-visible/MakeCharacterVisible.gd
+++ course/lesson-3-standing-on-shoulders-of-giants/make-visible/MakeCharacterVisible.gd
@@ -5,3 +5,5 @@ func _run():
 	# EXPORT show
 	show()
 	# /EXPORT show
+	yield(get_tree().create_timer(1.0), "timeout")
+	Events.emit_signal("practice_run_completed")
