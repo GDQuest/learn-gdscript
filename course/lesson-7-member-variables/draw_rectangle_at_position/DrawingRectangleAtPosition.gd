d3fb37db21f96202cc1aae3b005aeadf8a59b5d7
johnnygossdev <me@johnnygoss.dev>
2022-01-21 14:34:47 +0000

    improvement: lesson 7 pass

--- course/lesson-7-member-variables/draw_rectangle_at_position/DrawingRectangleAtPosition.gd
+++ course/lesson-7-member-variables/draw_rectangle_at_position/DrawingRectangleAtPosition.gd
@@ -23,3 +23,13 @@ func test_assignment():
 	position.y = 100
 	draw_rectangle(200, 120)
 	# /EXPORT test_assignment
+
+
+func _ready() -> void:
+	if not is_connected("turtle_finished", self, "_complete_run"):
+		connect("turtle_finished", self, "_complete_run")
+
+
+func _complete_run() -> void:
+	yield(get_tree().create_timer(0.5), "timeout")
+	Events.emit_signal("practice_run_completed")
