557d14b53160ba735d95f8e997e930c875a3b0b2
johnnygossdev <me@johnnygoss.dev>
2022-02-03 16:39:10 +0000

    content: add practice 2

---
+++ course/lesson-16-2d-vectors/resetting_robot/RobotReset.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+# Lowers the chance of the student brute forcing using -/+
+var _position_start = Vector2(600.5413, 560.98476)
+var _scale_start = Vector2(5.154, 5.154)
+
+onready var _animation_player := $AnimationPlayer
+
+func _ready() -> void:
+	position = _position_start
+	scale = _scale_start
+
+# EXPORT reset
+func reset():
+	scale = Vector2(1.0, 1.0)
+	position = Vector2(0.0, 0.0)
+# /EXPORT reset
+	position += Vector2(320, 240)
+
+func _run() -> void:
+	reset()
+	yield(get_tree().create_timer(1), "timeout")
+	Events.emit_signal("practice_run_completed")
