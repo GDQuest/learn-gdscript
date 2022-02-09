46af94fb63b1800dadd4c46bb46d86b8ac26ebeb
Nathan Lovato <nathan@gdquest.com>
2022-02-09 19:25:15 +0100

    fix: prevent lesson 11 practices from passing when multiplying after call
    fix #289

--- course/lesson-11-time-delta/rotating-and-moving-delta/TestsRotatingMovingDelta.gd
+++ course/lesson-11-time-delta/rotating-and-moving-delta/TestsRotatingMovingDelta.gd
@@ -3,9 +3,10 @@ extends PracticeTester
 
 func test_moving_in_a_circle_is_time_dependent() -> String:
 	var has_delta := _slice.current_text.rfind("delta") > 0
-	var has_spaces := _slice.current_text.rfind(" * ") > 0
+	var has_multiplication_sign := _slice.current_text.rfind("*") > 0
 	var has_two := _slice.current_text.rfind("2") > 0
 	var has_hundred := _slice.current_text.rfind("100") > 0
+	var ends_width_parenthesis := _slice.current_text.strip_edges().rfind(")")
 
 	if not has_delta:
 		return "Did you use delta to make the rotation time-dependent?"
@@ -13,7 +14,8 @@ func test_moving_in_a_circle_is_time_dependent() -> String:
 		return "Is the rotation speed correct?"
 	elif not has_hundred:
 		return "Is the movement speed correct?"
-	elif not has_spaces:
+	elif not has_multiplication_sign:
 		return "Did you use spaces between *?"
-
+	elif not ends_width_parenthesis:
+		return "Did you not multiply by delta inside the function call? You need to multiply inside the parentheses for delta to take effect."
 	return ""
--- course/lesson-11-time-delta/rotating-using-delta/TestsRotatingDelta.gd
+++ course/lesson-11-time-delta/rotating-using-delta/TestsRotatingDelta.gd
@@ -3,13 +3,16 @@ extends PracticeTester
 
 func test_rotating_character_is_time_dependent() -> String:
 	var has_delta := _slice.current_text.rfind("delta") > 0
-	var has_spaces := _slice.current_text.rfind(" * ") > 0
+	var has_multiplication_sign := _slice.current_text.rfind("*") > 0
 	var has_two := _slice.current_text.rfind("2") > 0
+	var is_in_parentheses := _slice.current_text.strip_edges.rfind(")")
 
 	if not has_delta:
 		return "Did you use delta to make the rotation time-dependent?"
 	elif not has_two:
 		return "Is the rotation speed correct?"
-	elif not has_spaces:
-		return "Did you use spaces between *?"
+	elif not has_multiplication_sign:
+		return "We couldn't find a multiplication sign. Did you use it to make the rotation time-dependent?"
+	elif not is_in_parentheses:
+		return "Did you not multiply by delta inside the function call? You need to multiply inside the parentheses for delta to take effect."
 	return ""
