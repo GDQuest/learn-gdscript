0f506c4a0ecb55d2090bd411f122459d1cb0618a
johnnygossdev <me@johnnygoss.dev>
2022-02-11 11:15:45 +0000

    add placeholder tests to practice 1

--- course/lesson-17-while-loops/while_battle/TestWhileBattle.gd
+++ course/lesson-17-while-loops/while_battle/TestWhileBattle.gd
@@ -3,25 +3,15 @@ extends PracticeTester
 var first_node: Node2D
 var turtle_power
 
+
 func _prepare() -> void:
 	first_node = _scene_root_viewport.get_child(0)
 	turtle_power = first_node.get("turtle_power")
 
-func _robot_health_reduced_to_zero() -> String:
-	return ""
-
-func robot_health_reduced_to_zero() -> String:
-	var regex = RegEx.new()
-	regex.compile("scale\\s*\\+.*Vector2|scale\\s*\\-.*Vector2|Vector2.*\\+.*scale")
-	var result = regex.search(_slice.current_text)
-	if not result:
-		return "It looks like the scale isn't increasing by some vector. Did you add a vector to scale?"
-	return ""
-
 
 func test_robot_health_reduced_to_zero() -> String:
-	
-	if turtle_power * 100 >= 100:
-		return ""
+	return "Test not implemented yet."
 
-	return "Health is too high."
+
+func test_use_while_loop() -> String:
+	return "Test not implemented yet."
