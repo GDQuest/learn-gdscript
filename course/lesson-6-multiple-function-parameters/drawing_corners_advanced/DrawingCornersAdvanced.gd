2e897bd26662adc35d2a6d0332caa28a73166b18
Nathan Lovato <nathan@gdquest.com>
2022-02-12 23:14:21 +0100

    chore: rework corner practices after turtle rewrite

--- course/lesson-6-multiple-function-parameters/drawing_corners_advanced/DrawingCornersAdvanced.gd
+++ course/lesson-6-multiple-function-parameters/drawing_corners_advanced/DrawingCornersAdvanced.gd
@@ -6,13 +6,10 @@ var _length := 240
 func _run():
 	reset()
 	draw_corner(_length, 45)
+	turn_left(45)
 	jump(-340, -80)
 	_length /= 2
-	rotation_degrees = 0
 	draw_corner(_length, 90)
-	jump(-160, -40)
-	rotation_degrees = 0
-	draw_corner(_length, 135)
 	play_draw_animation()
 
 
