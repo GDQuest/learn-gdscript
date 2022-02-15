105f26c1f422f461e6615b2def601d7b4374e445
johnnygossdev <me@johnnygoss.dev>
2022-02-15 17:41:42 +0000

    redo practice 1

---
+++ course/lesson-17-while-loops/while_move/TestGridWhilePractice.gd
@@ -0,0 +1,15 @@
+extends PracticeTester
+
+var first_node: Node2D
+
+
+func _prepare() -> void:
+	first_node = _scene_root_viewport.get_child(0)
+
+
+func test_robot_gets_to_bottom_of_board() -> String:
+	return "Test not implemented yet."
+
+
+func test_use_while_loop() -> String:
+	return "Test not implemented yet."
