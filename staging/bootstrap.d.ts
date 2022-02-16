b20a930534ca0fc892789b6a0b45d23641078ad6
NathanLovato <nathan@gdquest.com>
2022-02-16 11:13:48 +0000

    Deploying to gh-pages from  @ 64fb16ebf22406825099ae90c8c59d06b89bad9f 🚀

--- staging/bootstrap.d.ts
+++ staging/bootstrap.d.ts
@@ -1,65 +1,8 @@
 interface GDQuestLib {
   startLoading: () => void;
   displayFailureNotice: (err: Error | string) => void;
-  log: Log;
-  makeLogger: (title: string) => Console;
-  fullScreen: {
-    isIt: {
-      checkFullScreenElement: () => boolean;
-      checkCSSMediaQuery: () => boolean;
-      checkWindowMargins: () => boolean;
-    };
-    toggle: () => void;
-  };
-  events: {
-    onError: Signal;
-    onFullScreen: Signal;
-    onGodotLoaded: Signal;
-    onResize: Signal;
-  };
 }
 
-declare const GDQUEST_ENVIRONMENT: {
-  github_repository: string;
-  github_workflow: string;
-  github_ref_name: string;
-  github_sha: string;
-  override_file: string;
-  sub_build_path: string;
-  watermark: string;
-};
-
-interface Signal {
-  disconnect: (fn: (...args: any[]) => void) => boolean;
-  connect: (fn: (...args: any[]) => void) => () => boolean;
-  once: (fn: (...args: any[]) => void) => () => boolean;
-  emit: (...args: any[]) => void;
-}
-
-interface LogFunction {
-  (object: any, msg: string): void;
-}
-
-interface LogLine extends Record<string, any> {
-  time: number;
-  level: number;
-  msg: string;
-}
-
-interface Log {
-  trace: LogFunction;
-  debug: LogFunction;
-  info: LogFunction;
-  warn: LogFunction;
-  error: LogFunction;
-  fatal: LogFunction;
-  display: () => void;
-  clear: () => void;
-  download: () => void;
-  trimIfOverLimit: (maxKiloBytes?: number) => boolean;
-  logSystemInfoIfLogIsEmpty: (additionalData?: Record<string, any>) => void;
-  get: () => LogLine[];
-}
 interface GodotEngineInstanceStartGameOptions {
   onProgress: (current: number, total: number) => void;
 }
@@ -70,17 +13,6 @@ declare class GodotEngineInstance {
 
 declare const GODOT_CONFIG: {
   canvasResizePolicy: number;
-  unloadAfterInit: boolean;
-  canvas: HTMLCanvasElement;
-  executable: string;
-  mainPack: string;
-  locale: string;
-  args: string[];
-  onExecute: (path: string, args: string[]) => void;
-  onExit: (status_code: number) => void;
-  onProgress: (current: number, total: number) => void;
-  onPrint: (...args: any[]) => void;
-  onPrintError: (...args: any[]) => void;
 };
 
 declare const Engine: {
