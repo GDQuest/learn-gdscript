4627a53dab3e2b1df1830dcbc65312f808c59d41
Yuri Sizov <yuris@humnom.net>
2022-02-18 17:27:12 +0300

    feat: add translation message extraction for error and classref databases

--- i18n/extract.py
+++ i18n/extract.py
@@ -1,4 +1,7 @@
+from typing import List
+
 import os
+import csv
 from babel.messages import extract
 from babel.messages import Catalog
 from babel.messages import pofile
@@ -36,13 +39,12 @@ def extract_application_messages() -> None:
 		"tr": None,
 	}
 
-	extract_and_write(
+	extract_babel_and_write(
 		globs_map=globs_map,
 		options_map=options_map,
 		keywords=keywords,
 		output_file="./i18n/application.pot",
 	)
-
 
 
 def extract_course_messages() -> None:
@@ -85,7 +87,7 @@ def extract_lesson_messages(lesson: str) -> None:
 		"Resource/hints": None,
 	}
 
-	extract_and_write(
+	extract_babel_and_write(
 		globs_map=globs_map,
 		options_map=options_map,
 		keywords=keywords,
@@ -93,7 +95,7 @@ def extract_lesson_messages(lesson: str) -> None:
 	)
 
 
-def extract_and_write(
+def extract_babel_and_write(
 	globs_map,
 	options_map,
 	keywords,
@@ -101,6 +103,13 @@ def extract_and_write(
 ) -> None:
 
 	print("  Starting extraction...")
+	cat = Catalog(
+		project=PROJECT,
+		version=VERSION,
+		copyright_holder=COPYRIGHT_HOLDER,
+		msgid_bugs_address=BUGS_ADDRESS
+	)
+
 	extractor = extract.extract_from_dir(
 		dirname=".",
 		method_map=globs_map,
@@ -109,13 +118,6 @@ def extract_and_write(
 		comment_tags=(),
 		callback=_log_extraction_file,
 		strip_comment_tags=False
-	)
-
-	cat = Catalog(
-		project=PROJECT,
-		version=VERSION,
-		copyright_holder=COPYRIGHT_HOLDER,
-		msgid_bugs_address=BUGS_ADDRESS
 	)
 
 	# (filename, lineno, message, comments, context)
@@ -140,6 +142,80 @@ def extract_and_write(
 	print("  Finished extraction.")
 
 
+def extract_error_database() -> None:
+	print("Reading error database messages...")
+
+	extracted_fields = [
+		"error_explanation",
+		"error_suggestion",
+	]
+
+	extract_csv_and_write(
+		source_file="./lsp/error_database.csv",
+		extract_fields=extracted_fields,
+		reference_field="error_code",
+		output_file="./i18n/error_database.pot",
+	)
+
+
+def extract_classref_database() -> None:
+	print("Reading classref database messages...")
+
+	extracted_fields = [
+		"explanation",
+	]
+
+	extract_csv_and_write(
+		source_file="./course/documentation.csv",
+		extract_fields=extracted_fields,
+		reference_field="identifier",
+		output_file="./i18n/classref_database.pot",
+	)
+
+
+def extract_csv_and_write(
+	source_file: str,
+	extract_fields: List[str],
+	reference_field: str,
+	output_file: str,
+) -> None:
+
+	print("  Starting extraction...")
+	cat = Catalog(
+		project=PROJECT,
+		version=VERSION,
+		copyright_holder=COPYRIGHT_HOLDER,
+		msgid_bugs_address=BUGS_ADDRESS
+	)
+
+	with open(source_file, "r", newline='') as cvsfile:
+		reader = csv.DictReader(cvsfile, delimiter=',', quotechar='"', skipinitialspace=True)
+
+		for row in reader:
+			for field_name in extract_fields:
+				message_id = row[field_name]
+				if not message_id:
+					continue
+
+				message_id = message_id.replace("\r\n", "\n")
+
+				cat.add(
+					id=message_id,
+					string="",
+					locations=[(source_file, reader.line_num)],
+					auto_comments=[ "Reference: " + row[reference_field] ],
+					context=None,
+				)
+
+	with open(output_file, "wb") as file:
+		pofile.write_po(
+			fileobj=file,
+			catalog=cat,
+		)
+
+	print("  Finished extraction.")
+
+
 def _log_extraction_file(filename, method, options):
 	print("  Extracting from file '" + filename + "'")
 
@@ -147,6 +223,8 @@ def _log_extraction_file(filename, method, options):
 def main():
 	extract_application_messages()
 	extract_course_messages()
+	extract_error_database()
+	extract_classref_database()
 
 
 if __name__ == "__main__":
