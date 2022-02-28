b02b9bc5c22a5727ee0b1b04b643e63d252d968d
Răzvan C. Rădulescu <razcore.rad@gmail.com>
2022-02-28 16:54:01 +0200

    fix(lesson10): Pick correct node to check in validation script

--- course/lesson-10-the-game-loop/rotating-sprite/TestsRotating.gd
+++ course/lesson-10-the-game-loop/rotating-sprite/TestsRotating.gd
@@ -2,8 +2,7 @@ extends PracticeTester
 
 
 func test_character_is_rotating_clockwise() -> String:
-	var node_2d = _scene_root_viewport.get_child(0).get_child(0)
-	var node_rotation = node_2d.rotation
-	if node_rotation < 0.0:
+	var robot: Node2D = _scene_root_viewport.get_child(0).get_node("Robot")
+	if robot.rotation < 0.0:
 		return tr("The robot is turning in the wrong direction!")
 	return ""
