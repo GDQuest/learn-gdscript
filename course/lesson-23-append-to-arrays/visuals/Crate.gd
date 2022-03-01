a129d00d1b4272bdb611bf9e97d0491561c284aa
Xananax <me@xananax.com>
2022-03-01 17:04:36 +0400

    improvement: gems can change color; restore indices

--- course/lesson-23-append-to-arrays/visuals/Crate.gd
+++ course/lesson-23-append-to-arrays/visuals/Crate.gd
@@ -3,13 +3,32 @@ extends Panel
 signal used
 signal restored
 
+const FIRE := preload("res://course/lesson-24-access-array-indices/visuals/inventory/pickup_fire.png")
+const GEM := preload("res://course/lesson-24-access-array-indices/visuals/inventory/pickup_gem.png")
+const HEALTH := preload("res://course/lesson-24-access-array-indices/visuals/inventory/pickup_health.png")
+const ICE := preload("res://course/lesson-24-access-array-indices/visuals/inventory/pickup_ice.png")
+const LIGHTNING := preload("res://course/lesson-24-access-array-indices/visuals/inventory/pickup_lightning.png")
+
+const textures = [
+	FIRE,
+	GEM,
+	HEALTH,
+	ICE,
+	LIGHTNING
+]
+
+var texture: Texture setget set_texture, get_texture
+
 onready var anim_player := $AnimationPlayer as AnimationPlayer
 onready var texture_rect := $TextureRect as TextureRect
 onready var label := $Label as Label
+
 var _animation_backwards := false
 
+
 func _ready() -> void:
-	label.text = str(get_index())
+	if texture == null:
+		set_texture(textures[randi() % textures.size()])
 	anim_player.connect("animation_finished", self, "_on_animation_finished")
 
 
@@ -35,3 +54,22 @@ func _on_animation_finished(animation_name: String) -> void:
 		return
 	hide()
 	emit_signal("used")
+
+
+func set_texture(new_texture: Texture) -> void:
+	texture = new_texture
+	if not is_inside_tree():
+		yield(self, "ready")
+	texture_rect.texture = new_texture
+	
+func get_texture() -> Texture:
+	return texture
+
+func get_texture_name():
+	var path := texture.resource_path
+	var filename := path.get_file().get_basename().split("_")
+	filename.invert()
+	return PoolStringArray(filename).join(" ")
+
+func set_label_index(index: int) -> void:
+	label.text = str(index)
--- course/lesson-23-append-to-arrays/visuals/CrateStack.gd
+++ course/lesson-23-append-to-arrays/visuals/CrateStack.gd
@@ -8,9 +8,21 @@ var crates := []
 var _is_resetting := false
 
 
+const code = """
+var crates = ["%s"]
+
+func use_top_crate():
+	var crate = crates.pop_back()
+	use(crate)
+"""
+
+
 func _ready() -> void:
 	_initial_crates.invert()
+	var i:= 0
 	for crate in _initial_crates:
+		crate.set_label_index(i)
+		i += 1
 		crate.connect("restored", self, "restore_crate")
 	crates = range(_initial_crates.size())
 
@@ -24,7 +36,16 @@ func _sync_nodes():
 	var index := crates.size()
 	if index < 0:
 		return
-	_initial_crates[index].use()
+	var crate = _initial_crates[index]
+	crate.use()
+	var remaining = PoolStringArray()
+	for crate_index in crates:
+		remaining.append(_initial_crates[crate_index].get_texture_name())
+	# TODO: display this to the user?
+	prints(
+		"removed: ", crate.get_texture_name(), 
+		"remaining: ", '["'+remaining.join('", "')+'"]'
+	)
 
 
 func reset():
@@ -38,3 +59,10 @@ func restore_crate():
 	elif index < _initial_crates.size():
 		crates.append(index)
 		_initial_crates[index].reset()
+
+
+func get_code(_initial_code: String) -> String:
+	var names := PoolStringArray()
+	for crate in _initial_crates:
+		names.append(crate.get_texture_name())
+	return code % [names.join('", "')]
