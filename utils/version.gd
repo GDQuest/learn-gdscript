2bd2422653f9114a80531076b6f0eec7a0ef8463
Răzvan C. Rădulescu <razcore.rad@gmail.com>
2022-03-02 14:12:44 +0200

    chore: add version info for desktop platforms
    It also handles mouse clicks:
    
    - hash goes to github hash version
    - version number goes to the changelog file
    
    closes #375
    closes #376

--- utils/version.gd
+++ utils/version.gd
@@ -1,9 +1,9 @@
-# AUTO GENERATED FILE, YOUR CHANGES WILL NOT REMAIN
+# AUTO GENERATED changed_file, YOUR CHANGES WILL NOT REMAIN
 class_name AppVersion
 
 const version := "0.3.0";
-const git_commit := "e5d041daa4fa7eaa6932d1ae02fdb97782678eeb";
-const git_branch := "main";
-const build_date := [2022, 02, 14, 18, 45, 09];
-const build_date_unix := 1644849909;
-const build_date_iso := "2022/02/14 18:45:09;"
+const git_commit := "6de164cb076cb7ae64eabd4e9e28fdcc865e3840";
+const git_branch := "version-number";
+const build_date := [2022, 03, 02, 14, 11, 31];
+const build_date_unix := 1646223091;
+const build_date_iso := "2022/03/02 14:11:31;"
