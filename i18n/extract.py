215c57174260fb89f488c7dd4b0df839e7ea59ef
Nathan Lovato <nathan@gdquest.com>
2022-03-03 16:00:53 +0100

    feat: add course glossary

--- i18n/extract.py
+++ i18n/extract.py
@@ -13,224 +13,233 @@ BUGS_ADDRESS = "https://github.com/GDQuest/learn-gdscript"
 
 
 def extract_application_messages() -> None:
-	print("Reading application messages...")
+    print("Reading application messages...")
 
-	globs_map = [
-		('resources/**/**.gd', 'python'),
-		('ui/**/**.gd', 'python'),
-		('ui/**/**.tscn', 'godot_scene'),
-	]
-	options_map = {
-		'resources/**/**.gd': {
-			'encoding': 'utf-8'
-		},
-		'ui/**/**.gd': {
-			'encoding': 'utf-8'
-		},
-		'ui/**/**.tscn': {
-			'encoding': 'utf-8'
-		},
-	}
+    globs_map = [
+        ("resources/**/**.gd", "python"),
+        ("ui/**/**.gd", "python"),
+        ("ui/**/**.tscn", "godot_scene"),
+    ]
+    options_map = {
+        "resources/**/**.gd": {"encoding": "utf-8"},
+        "ui/**/**.gd": {"encoding": "utf-8"},
+        "ui/**/**.tscn": {"encoding": "utf-8"},
+    }
 
-	keywords = {
-		# Properties stored in scenes.
-		"Label/text": None,
-		"Button/text": None,
-		"RichTextLabel/bbcode_text": None,
-		"LineEdit/placeholder_text": None,
-		"title": None,
+    keywords = {
+        # Properties stored in scenes.
+        "Label/text": None,
+        "Button/text": None,
+        "RichTextLabel/bbcode_text": None,
+        "LineEdit/placeholder_text": None,
+        "title": None,
+        # Code-based translated strings
+        "tr": None,
+    }
 
-		# Code-based translated strings
-		"tr": None,
-	}
-
-	extract_babel_and_write(
-		globs_map=globs_map,
-		options_map=options_map,
-		keywords=keywords,
-		output_file="./i18n/application.pot",
-	)
+    extract_babel_and_write(
+        globs_map=globs_map,
+        options_map=options_map,
+        keywords=keywords,
+        output_file="./i18n/application.pot",
+    )
 
 
 def extract_course_messages() -> None:
-	lessons_directory = "./course"
-	for filename in os.listdir(lessons_directory):
-		full_path = os.path.join(lessons_directory, filename)
-		if filename.startswith("lesson-") and os.path.isdir(full_path):
-			extract_lesson_messages(lesson=filename)
+    lessons_directory = "./course"
+    for filename in os.listdir(lessons_directory):
+        full_path = os.path.join(lessons_directory, filename)
+        if filename.startswith("lesson-") and os.path.isdir(full_path):
+            extract_lesson_messages(lesson=filename)
 
 
 def extract_lesson_messages(lesson: str) -> None:
-	print("Reading lesson messages from '" + 'course/' + lesson + '/lesson.tres' + "'...")
+    print(
+        "Reading lesson messages from '" + "course/" + lesson + "/lesson.tres" + "'..."
+    )
 
-	globs_map = [
-		('course/' + lesson + '/lesson.tres', 'godot_resource'),
-	]
-	options_map = {
-		'course/' + lesson + '/lesson.tres': {
-			'encoding': 'utf-8'
-		},
-	}
+    globs_map = [
+        ("course/" + lesson + "/lesson.tres", "godot_resource"),
+    ]
+    options_map = {
+        "course/" + lesson + "/lesson.tres": {"encoding": "utf-8"},
+    }
 
-	keywords = {
-		# Content blocks.
-		"Resource/title": None,
-		"Resource/text": None,
+    keywords = {
+        # Content blocks.
+        "Resource/title": None,
+        "Resource/text": None,
+        # Quizzes.
+        "Resource/question": None,
+        "Resource/hint": None,
+        "Resource/content_bbcode": None,
+        "Resource/explanation_bbcode": None,
+        "Resource/valid_answer": None,
+        "Resource/answer_options": None,
+        "Resource/valid_answers": None,
+        # Practices.
+        "Resource/goal": None,
+        "Resource/description": None,
+        "Resource/hints": None,
+    }
 
-		# Quizzes.
-		"Resource/question": None,
-		"Resource/hint": None,
-		"Resource/content_bbcode": None,
-		"Resource/explanation_bbcode": None,
-		"Resource/valid_answer": None,
-		"Resource/answer_options": None,
-		"Resource/valid_answers": None,
-
-		# Practices.
-		"Resource/goal": None,
-		"Resource/description": None,
-		"Resource/hints": None,
-	}
-
-	extract_babel_and_write(
-		globs_map=globs_map,
-		options_map=options_map,
-		keywords=keywords,
-		output_file="./i18n/" + lesson + ".pot",
-	)
+    extract_babel_and_write(
+        globs_map=globs_map,
+        options_map=options_map,
+        keywords=keywords,
+        output_file="./i18n/" + lesson + ".pot",
+    )
 
 
 def extract_babel_and_write(
-	globs_map,
-	options_map,
-	keywords,
-	output_file: str,
+    globs_map,
+    options_map,
+    keywords,
+    output_file: str,
 ) -> None:
 
-	print("  Starting extraction...")
-	cat = Catalog(
-		project=PROJECT,
-		version=VERSION,
-		copyright_holder=COPYRIGHT_HOLDER,
-		msgid_bugs_address=BUGS_ADDRESS
-	)
+    print("  Starting extraction...")
+    cat = Catalog(
+        project=PROJECT,
+        version=VERSION,
+        copyright_holder=COPYRIGHT_HOLDER,
+        msgid_bugs_address=BUGS_ADDRESS,
+    )
 
-	extractor = extract.extract_from_dir(
-		dirname=".",
-		method_map=globs_map,
-		options_map=options_map,
-		keywords=keywords,
-		comment_tags=(),
-		callback=_log_extraction_file,
-		strip_comment_tags=False
-	)
+    extractor = extract.extract_from_dir(
+        dirname=".",
+        method_map=globs_map,
+        options_map=options_map,
+        keywords=keywords,
+        comment_tags=(),
+        callback=_log_extraction_file,
+        strip_comment_tags=False,
+    )
 
-	# (filename, lineno, message, comments, context)
-	for message in extractor:
-		message_id = message[2]
-		message_id = message_id.replace("\r\n", "\n")
+    # (filename, lineno, message, comments, context)
+    for message in extractor:
+        message_id = message[2]
+        message_id = message_id.replace("\r\n", "\n")
 
-		cat.add(
-			id=message_id,
-			string="",
-			locations=[(message[0], message[1])],
-			auto_comments=message[3],
-			context=message[4],
-		)
+        cat.add(
+            id=message_id,
+            string="",
+            locations=[(message[0], message[1])],
+            auto_comments=message[3],
+            context=message[4],
+        )
 
-	with open(output_file, "wb") as file:
-		pofile.write_po(
-			fileobj=file,
-			catalog=cat,
-		)
+    with open(output_file, "wb") as file:
+        pofile.write_po(
+            fileobj=file,
+            catalog=cat,
+        )
 
-	print("  Finished extraction.")
+    print("  Finished extraction.")
 
 
 def extract_error_database() -> None:
-	print("Reading error database messages...")
+    print("Reading error database messages...")
 
-	extracted_fields = [
-		"error_explanation",
-		"error_suggestion",
-	]
+    extracted_fields = [
+        "error_explanation",
+        "error_suggestion",
+    ]
 
-	extract_csv_and_write(
-		source_file="./lsp/error_database.csv",
-		extract_fields=extracted_fields,
-		reference_field="error_code",
-		output_file="./i18n/error_database.pot",
-	)
+    extract_csv_and_write(
+        source_file="./lsp/error_database.csv",
+        extract_fields=extracted_fields,
+        reference_field="error_code",
+        output_file="./i18n/error_database.pot",
+    )
 
 
 def extract_classref_database() -> None:
-	print("Reading classref database messages...")
+    print("Reading classref database messages...")
 
-	extracted_fields = [
-		"explanation",
-	]
+    extracted_fields = [
+        "explanation",
+    ]
 
-	extract_csv_and_write(
-		source_file="./course/documentation.csv",
-		extract_fields=extracted_fields,
-		reference_field="identifier",
-		output_file="./i18n/classref_database.pot",
-	)
+    extract_csv_and_write(
+        source_file="./course/documentation.csv",
+        extract_fields=extracted_fields,
+        reference_field="identifier",
+        output_file="./i18n/classref_database.pot",
+    )
+
+
+def extract_glossary_database() -> None:
+    print("Reading glossary database messages...")
+
+    extracted_fields = [
+        "term",
+        "explanation",
+    ]
+
+    extract_csv_and_write(
+        source_file="./course/glossary.csv",
+        extract_fields=extracted_fields,
+        reference_field="term",
+        output_file="./i18n/glossary_database.pot",
+    )
 
 
 def extract_csv_and_write(
-	source_file: str,
-	extract_fields: List[str],
-	reference_field: str,
-	output_file: str,
+    source_file: str,
+    extract_fields: List[str],
+    reference_field: str,
+    output_file: str,
 ) -> None:
 
-	print("  Starting extraction...")
-	cat = Catalog(
-		project=PROJECT,
-		version=VERSION,
-		copyright_holder=COPYRIGHT_HOLDER,
-		msgid_bugs_address=BUGS_ADDRESS
-	)
+    print("  Starting extraction...")
+    cat = Catalog(
+        project=PROJECT,
+        version=VERSION,
+        copyright_holder=COPYRIGHT_HOLDER,
+        msgid_bugs_address=BUGS_ADDRESS,
+    )
 
-	with open(source_file, "r", newline='') as cvsfile:
-		reader = csv.DictReader(cvsfile, delimiter=',', quotechar='"', skipinitialspace=True)
+    with open(source_file, "r", newline="") as cvsfile:
+        reader = csv.DictReader(
+            cvsfile, delimiter=",", quotechar='"', skipinitialspace=True
+        )
 
-		for row in reader:
-			for field_name in extract_fields:
-				message_id = row[field_name]
-				if not message_id:
-					continue
+        for row in reader:
+            for field_name in extract_fields:
+                message_id = row[field_name]
+                if not message_id:
+                    continue
 
-				message_id = message_id.replace("\r\n", "\n")
+                message_id = message_id.replace("\r\n", "\n")
 
-				cat.add(
-					id=message_id,
-					string="",
-					locations=[(source_file, reader.line_num)],
-					auto_comments=[ "Reference: " + row[reference_field] ],
-					context=None,
-				)
+                cat.add(
+                    id=message_id,
+                    string="",
+                    locations=[(source_file, reader.line_num)],
+                    auto_comments=["Reference: " + row[reference_field]],
+                    context=None,
+                )
 
-	with open(output_file, "wb") as file:
-		pofile.write_po(
-			fileobj=file,
-			catalog=cat,
-		)
+    with open(output_file, "wb") as file:
+        pofile.write_po(
+            fileobj=file,
+            catalog=cat,
+        )
 
-	print("  Finished extraction.")
+    print("  Finished extraction.")
 
 
 def _log_extraction_file(filename, method, options):
-	print("  Extracting from file '" + filename + "'")
+    print("  Extracting from file '" + filename + "'")
 
 
 def main():
-	extract_application_messages()
-	extract_course_messages()
-	extract_error_database()
-	extract_classref_database()
+    extract_application_messages()
+    extract_course_messages()
+    extract_error_database()
+    extract_classref_database()
 
 
 if __name__ == "__main__":
-	main()
+    main()
---
+++ ui/components/GlossaryPopup.gd
@@ -0,0 +1,25 @@
+extends Node
+
+onready var _popup := $Panel as Control
+onready var _title := $Panel/MarginContainer/Column/Title as Label
+onready var _content := $Panel/MarginContainer/Column/Content as RichTextLabel
+
+
+func _ready() -> void:
+	_popup.hide()
+
+
+func setup(term: String, bbcode_text: String) -> void:
+	if not is_inside_tree():
+		yield(self, "ready")
+	_title.text = term
+	_content.bbcode_text = bbcode_text
+	_popup.connect("mouse_exited", _popup, "hide")
+
+
+func show():
+	_popup.show()
+
+
+func align_with_mouse(global_mouse_position: Vector2) -> void:
+	_popup.rect_global_position = global_mouse_position
