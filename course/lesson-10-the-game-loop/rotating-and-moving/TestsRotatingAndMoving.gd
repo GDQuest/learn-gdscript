23044f7caafe8f76e94318c827d0f4a2793b8ee2
Nathan Lovato <nathan@gdquest.com>
2022-03-07 11:23:35 +0100

    fix: prevent commented code lines from making L10 practices pass
    fix #429

--- course/lesson-10-the-game-loop/rotating-and-moving/TestsRotatingAndMoving.gd
+++ course/lesson-10-the-game-loop/rotating-and-moving/TestsRotatingAndMoving.gd
@@ -1,8 +1,29 @@
 extends PracticeTester
 
+var robot: Node2D
+var lines: PoolStringArray
+
+
+func _prepare() -> void:
+	robot = _scene_root_viewport.get_child(0).get_node("Robot")
+	for line in _slice.current_text.split("\n"):
+		line = line.strip_edges().replace(" ", "")
+		if not line.empty():
+			lines.push_back(line)
+
 
 func test_character_is_moving_in_a_circle_clockwise() -> String:
-	var robot: Node2D = _scene_root_viewport.get_child(0).get_node("Robot")
+	var has_rotate := false
+	var has_move_local := false
+	for line in lines:
+		has_rotate = has_rotate or line.begins_with("rotate(")
+		has_move_local = has_move_local or line.begins_with("move_local_x(")
+
+	if not has_rotate:
+		return tr("Did you use rotate() to make the sprite rotate?")
+	elif not has_move_local:
+		return tr("Did you use make_local_x() to make the sprite move locally?")
+
 	if robot.rotation < 0.0:
 		return tr("The robot is turning in the wrong direction!")
 	elif is_equal_approx(robot.position.x, 300.0):
--- course/lesson-10-the-game-loop/rotating-sprite/TestsRotating.gd
+++ course/lesson-10-the-game-loop/rotating-sprite/TestsRotating.gd
@@ -1,8 +1,25 @@
 extends PracticeTester
 
+var robot: Node2D
+var lines: PoolStringArray
+
+
+func _prepare() -> void:
+	robot = _scene_root_viewport.get_child(0).get_node("Robot")
+	for line in _slice.current_text.split("\n"):
+		line = line.strip_edges().replace(" ", "")
+		if not line.empty():
+			lines.push_back(line)
 
 func test_character_is_rotating_clockwise() -> String:
-	var robot: Node2D = _scene_root_viewport.get_child(0).get_node("Robot")
+	var has_rotate := false
+	for line in lines:
+		if line.begins_with("rotate("):
+			has_rotate = true
+			break
+
+	if not has_rotate:
+		return tr("Did you use rotate() to make the sprite rotate?")
 	if robot.rotation < 0.0:
 		return tr("The robot is turning in the wrong direction!")
 	return ""
