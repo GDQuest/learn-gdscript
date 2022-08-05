f214530fe8021d453d4b80e487baee1d25b15de4
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2022-08-05 10:03:41 +0200

    fix: check the whole path in lesson 20
    fix #568

--- course/lesson-20-looping-over-arrays/robot-path/TestRobotPath.gd
+++ course/lesson-20-looping-over-arrays/robot-path/TestRobotPath.gd
@@ -20,6 +20,14 @@ func test_robot_moves_along_blue_path() -> String:
 	if path_robot.size() == 0:
 		return tr("The robot didn't move at all. Did you call robot.move_to()?")
 
+	for i in path_source.size():
+		var expected_point: Vector2 = path_source[i]
+		if path_robot.size() <= i:
+			return tr("The robot moved fewer times than it should have. Did you use a loop and move once for each point in the robot_path array?")
+		var found_point: Vector2 = path_robot[i]
+		if found_point != expected_point:
+			return tr("The cell at index %s didn't match the blue path. We got %s but expected %s." % [i, found_point, expected_point])
+
 	for point in path_robot:
 		if point != Vector2.ZERO and not point in path_source:
 			return tr("The robot's movement path doesn't match the robot_path array. They should be the same.")
@@ -27,6 +35,6 @@ func test_robot_moves_along_blue_path() -> String:
 
 
 func test_code_uses_move_to() -> String:
-	if not "robot.move_to" in _slice.current_text:
+	if not "robot.move_to(" in _slice.current_text:
 		return tr("We found no call to the robot's move_to() function. Did you forget to call it?")
 	return ""
