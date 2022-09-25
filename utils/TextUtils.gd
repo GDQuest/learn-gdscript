112e9c4e04720a2239cfc108f62ab2792b41d09a
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2022-09-25 17:10:34 +0200

    improvement: highlight symbols in lessons
    fix: correct syntax highlighting on nested function calls
    
    close #675

--- utils/TextUtils.gd
+++ utils/TextUtils.gd
@@ -6,41 +6,66 @@ extends Reference
 const _REGEX_CACHE := {}
 
 # Regex pattern to find [code][/code] tags
-const REGEX_PATTERN_CODE = "\\[code\\].+?\\[\\/code\\]"
+const REGEX_PATTERN_CODE = "\\[code\\](.+?)\\[\\/code\\]"
 # Regex pattern to find the "func" literal
 const REGEX_PATTERN_FUNC = "(?<func>func)"
 # Regex pattern to find method calls (e.g. "draw(...)")
-const REGEX_PATTERN_METHOD_CALL = "(?<method_name>[^\\s\\[\\]]*)(?<method_args>\\(.*?\\))"
+const REGEX_PATTERN_METHOD_CALL = "(?<method_name>\\w[\\w\\d]*)(?<method_args>\\(.*?\\))"
 # Regex pattern for numbers (with no special characters on either side)
 const REGEX_PATTERN_NUMBER = "(?<number>[^#\\[\\]\\(\\)]?[0-9]++\\.?[0-9][^#\\[\\]\\(\\)]?)"
+const REGEX_SYMBOL = "(?<symbol>[a-zA-Z][a-zA-Z0-9_]+)"
 
 
 static func bbcode_add_code_color(bbcode_text := "") -> String:
 	if not _REGEX_CACHE:
-		_REGEX_CACHE["regex_bbcode_code"] = RegEx.new()
-		_REGEX_CACHE["regex_bbcode_func"] = RegEx.new()
-		_REGEX_CACHE["regex_bbcode_method_call"] = RegEx.new()
-		_REGEX_CACHE["regex_bbcode_number"] = RegEx.new()
-		_REGEX_CACHE["regex_bbcode_code"].compile(REGEX_PATTERN_CODE)
-		_REGEX_CACHE["regex_bbcode_func"].compile(REGEX_PATTERN_FUNC)
-		_REGEX_CACHE["regex_bbcode_method_call"].compile(REGEX_PATTERN_METHOD_CALL)
-		_REGEX_CACHE["regex_bbcode_number"].compile(REGEX_PATTERN_NUMBER)
+		_REGEX_CACHE["regex_code"] = RegEx.new()
+		_REGEX_CACHE["regex_func"] = RegEx.new()
+		_REGEX_CACHE["regex_method_call"] = RegEx.new()
+		_REGEX_CACHE["regex_number"] = RegEx.new()
+		_REGEX_CACHE["regex_symbol"] = RegEx.new()
 
-	var regex_matches : Array = _REGEX_CACHE["regex_bbcode_code"].search_all(bbcode_text)
+		_REGEX_CACHE["regex_code"].compile(REGEX_PATTERN_CODE)
+		_REGEX_CACHE["regex_func"].compile(REGEX_PATTERN_FUNC)
+		_REGEX_CACHE["regex_method_call"].compile(REGEX_PATTERN_METHOD_CALL)
+		_REGEX_CACHE["regex_number"].compile(REGEX_PATTERN_NUMBER)
+		_REGEX_CACHE["regex_symbol"].compile(REGEX_SYMBOL)
+
+	var regex_matches: Array = _REGEX_CACHE["regex_code"].search_all(bbcode_text)
 	var index_delta := 0
 	
+	var REGEX_REPLACE_MAP := {
+		"regex_func": "[color=#%s]$func[/color]" % CodeEditorEnhancer.COLOR_KEYWORD.to_html(false),
+		"regex_method_call":
+		"[color=#%s]$method_name[/color]$method_args" % CodeEditorEnhancer.COLOR_MEMBER.to_html(false),
+		"regex_number": "[color=#%s]$number[/color]" % CodeEditorEnhancer.COLOR_NUMBERS.to_html(false),
+		"regex_symbol": "[color=#%s]$symbol[/color]" % CodeEditorEnhancer.COLOR_MEMBER.to_html(false)
+	}
+
 	for regex_match in regex_matches:
 		var index_offset = regex_match.get_start() + index_delta
-		var match_string : String = regex_match.strings[0]
-		var initial_length := match_string.length()
-		
-		match_string = _REGEX_CACHE["regex_bbcode_func"].sub(match_string, "[color=#%s]$func[/color]" % CodeEditorEnhancer.COLOR_KEYWORD.to_html(false))
-		match_string = _REGEX_CACHE["regex_bbcode_method_call"].sub(match_string, "[color=#%s]$method_name[/color]$method_args" % CodeEditorEnhancer.COLOR_MEMBER.to_html(false))
-		match_string = _REGEX_CACHE["regex_bbcode_number"].sub(match_string, "[color=#%s]$number[/color]" % CodeEditorEnhancer.COLOR_NUMBERS.to_html(false))
-		
+		var initial_length: int = regex_match.strings[0].length()
+		var match_string: String = regex_match.strings[1]
+
+		var current_index := 0
+		var last_closing_bracket_index := 0
+		var colored_string := ""
+		for regex_type in [
+			"regex_func",
+			"regex_method_call",
+			"regex_number",
+			"regex_symbol",
+		]:
+			var before := match_string.substr(current_index)
+			var replaced: String = _REGEX_CACHE[regex_type].sub(match_string, REGEX_REPLACE_MAP[regex_type], false, current_index)
+			if replaced != before:
+				last_closing_bracket_index = match_string.rfind("]")
+				colored_string += replaced.substr(current_index, last_closing_bracket_index)
+				current_index = last_closing_bracket_index
+
+		colored_string = "[code]" + colored_string + "[/code]"
 		bbcode_text.erase(index_offset, initial_length)
-		bbcode_text = bbcode_text.insert(index_offset, match_string)
-		index_delta += (match_string.length() - initial_length)
+		bbcode_text = bbcode_text.insert(index_offset, colored_string)
+		index_delta += (colored_string.length() - initial_length)
 
 	return bbcode_text
 
@@ -113,4 +138,3 @@ static func convert_type_index_to_text(type: int) -> String:
 		_:
 			printerr("Type value %s should be a member of the TYPE_* enum, but it is not.")
 	return "[ERROR, nonexistent type value %s]" % type
-
