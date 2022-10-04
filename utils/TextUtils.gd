a4550f43487cd249c670bac42d15d44005bade55
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2022-10-04 18:17:24 +0200

    refactor: make TextUtils an autoload

--- utils/TextUtils.gd
+++ utils/TextUtils.gd
@@ -1,9 +1,4 @@
-class_name TextUtils
-extends Reference
-
-# It's a trick to have a static instance of RegEx.
-# Bad bad hacks, but it does the job and static class > autoload helper.
-const _REGEX_CACHE := {}
+extends Node
 
 # Regex pattern to find [code][/code] tags
 const REGEX_PATTERN_CODE = "\\[code\\](.+?)\\[\\/code\\]"
@@ -15,31 +10,70 @@ const REGEX_PATTERN_METHOD_CALL = "(?<method_name>\\w[\\w\\d]*)(?<method_args>\\
 const REGEX_PATTERN_NUMBER = "(?<number>\\d+(\\.\\d+)?)"
 const REGEX_SYMBOL = "(?<symbol>[a-zA-Z][a-zA-Z0-9_]+)"
 
+# Maps type indices to their string representation.
+const TYPE_MAP := {
+	TYPE_BOOL: "boolean",
+	TYPE_INT: "whole number",
+	TYPE_REAL: "decimal number",
+	TYPE_STRING: "text string",
+	TYPE_VECTOR2: "2D vector",
+	TYPE_RECT2: "2D rectangle",
+	TYPE_VECTOR3: "3D vector",
+	TYPE_TRANSFORM2D: "2D transform",
+	TYPE_PLANE: "plane",
+	TYPE_QUAT: "quaternion",
+	TYPE_AABB: "axis-aligned bounding box",
+	TYPE_BASIS: "basis",
+	TYPE_TRANSFORM: "3D transform",
+	TYPE_COLOR: "color",
+	TYPE_NODE_PATH: "node path",
+	TYPE_RID: "resource's unique ID",
+	TYPE_OBJECT: "object",
+	TYPE_DICTIONARY: "dictionary",
+	TYPE_ARRAY: "array",
+	TYPE_RAW_ARRAY: "PoolByteArray",
+	TYPE_INT_ARRAY: "PoolIntArray",
+	TYPE_REAL_ARRAY: "PoolRealArray",
+	TYPE_STRING_ARRAY: "PoolStringArray",
+	TYPE_VECTOR2_ARRAY: "PoolVector2Array",
+	TYPE_VECTOR3_ARRAY: "PoolVector3Array",
+	TYPE_COLOR_ARRAY: "PoolColorArray",
+}
 
-static func bbcode_add_code_color(bbcode_text := "") -> String:
-	if not _REGEX_CACHE:
-		_REGEX_CACHE["regex_code"] = RegEx.new()
-		_REGEX_CACHE["regex_func"] = RegEx.new()
-		_REGEX_CACHE["regex_method_call"] = RegEx.new()
-		_REGEX_CACHE["regex_number"] = RegEx.new()
-		_REGEX_CACHE["regex_symbol"] = RegEx.new()
+# Caches regexes to highlight code in text.
+const _REGEX_CACHE := {}
+# Intended to be used as a constant
+var _REGEX_REPLACE_MAP := {}
 
-		_REGEX_CACHE["regex_code"].compile(REGEX_PATTERN_CODE)
-		_REGEX_CACHE["regex_func"].compile(REGEX_PATTERN_FUNC)
-		_REGEX_CACHE["regex_method_call"].compile(REGEX_PATTERN_METHOD_CALL)
-		_REGEX_CACHE["regex_number"].compile(REGEX_PATTERN_NUMBER)
-		_REGEX_CACHE["regex_symbol"].compile(REGEX_SYMBOL)
 
-	var regex_matches: Array = _REGEX_CACHE["regex_code"].search_all(bbcode_text)
-	var index_delta := 0
-	
-	var REGEX_REPLACE_MAP := {
-		"regex_func": "[color=#%s]$func[/color]" % CodeEditorEnhancer.COLOR_KEYWORD.to_html(false),
-		"regex_method_call":
-		"[color=#%s]$method_name[/color]$method_args" % CodeEditorEnhancer.COLOR_MEMBER.to_html(false),
-		"regex_number": "[color=#%s]$number[/color]" % CodeEditorEnhancer.COLOR_NUMBERS.to_html(false),
-		"regex_symbol": "[color=#%s]$symbol[/color]" % CodeEditorEnhancer.COLOR_MEMBER.to_html(false)
+func _init() -> void:
+	_REGEX_CACHE["code"] = RegEx.new()
+	_REGEX_CACHE["func"] = RegEx.new()
+	_REGEX_CACHE["method_call"] = RegEx.new()
+	_REGEX_CACHE["number"] = RegEx.new()
+	_REGEX_CACHE["symbol"] = RegEx.new()
+
+	_REGEX_CACHE["code"].compile(REGEX_PATTERN_CODE)
+	_REGEX_CACHE["func"].compile(REGEX_PATTERN_FUNC)
+	_REGEX_CACHE["method_call"].compile(REGEX_PATTERN_METHOD_CALL)
+	_REGEX_CACHE["number"].compile(REGEX_PATTERN_NUMBER)
+	_REGEX_CACHE["symbol"].compile(REGEX_SYMBOL)
+
+	_REGEX_REPLACE_MAP = {
+		"func": "[color=#%s]$func[/color]" % CodeEditorEnhancer.COLOR_KEYWORD.to_html(false),
+		"method_call":
+		(
+			"[color=#%s]$method_name[/color]$method_args"
+			% CodeEditorEnhancer.COLOR_MEMBER.to_html(false)
+		),
+		"number": "[color=#%s]$number[/color]" % CodeEditorEnhancer.COLOR_NUMBERS.to_html(false),
+		"symbol": "[color=#%s]$symbol[/color]" % CodeEditorEnhancer.COLOR_MEMBER.to_html(false)
 	}
+
+
+func bbcode_add_code_color(bbcode_text := "") -> String:
+	var regex_matches: Array = _REGEX_CACHE["code"].search_all(bbcode_text)
+	var index_delta := 0
 
 	for regex_match in regex_matches:
 		var index_offset = regex_match.get_start() + index_delta
@@ -50,13 +84,15 @@ static func bbcode_add_code_color(bbcode_text := "") -> String:
 		var last_closing_bracket_index := 0
 		var colored_string := ""
 		for regex_type in [
-			"regex_func",
-			"regex_method_call",
-			"regex_number",
-			"regex_symbol",
+			"func",
+			"method_call",
+			"number",
+			"symbol",
 		]:
 			var before := match_string.substr(current_index)
-			var replaced: String = _REGEX_CACHE[regex_type].sub(match_string, REGEX_REPLACE_MAP[regex_type], false, current_index)
+			var replaced: String = _REGEX_CACHE[regex_type].sub(
+				match_string, _REGEX_REPLACE_MAP[regex_type], false, current_index
+			)
 			if replaced != before:
 				last_closing_bracket_index = match_string.rfind("]")
 				colored_string += replaced.substr(current_index, last_closing_bracket_index)
@@ -83,60 +119,9 @@ static func convert_input_action_to_tooltip(action: String) -> String:
 	return output
 
 
-static func convert_type_index_to_text(type: int) -> String:
-	match type:
-		TYPE_BOOL:
-			return "boolean"
-		TYPE_INT:
-			return "whole number"
-		TYPE_REAL:
-			return "decimal number"
-		TYPE_STRING:
-			return "text string"
-		TYPE_VECTOR2:
-			return "2D vector"
-		TYPE_RECT2:
-			return "2D rectangle"
-		TYPE_VECTOR3:
-			return "3D vector"
-		TYPE_TRANSFORM2D:
-			return "2D transform"
-		TYPE_PLANE:
-			return "plane"
-		TYPE_QUAT:
-			return "quaternion"
-		TYPE_AABB:
-			return "axis-aligned bounding box"
-		TYPE_BASIS:
-			return "basis"
-		TYPE_TRANSFORM:
-			return "3D transform"
-		TYPE_COLOR:
-			return "color"
-		TYPE_NODE_PATH:
-			return "node path"
-		TYPE_RID:
-			return "resource's unique ID"
-		TYPE_OBJECT:
-			return "object"
-		TYPE_DICTIONARY:
-			return "dictionary"
-		TYPE_ARRAY:
-			return "array"
-		TYPE_RAW_ARRAY:
-			return "PoolByteArray"
-		TYPE_INT_ARRAY:
-			return "PoolIntArray"
-		TYPE_REAL_ARRAY:
-			return "PoolRealArray"
-		TYPE_STRING_ARRAY:
-			return "PoolStringArray"
-		TYPE_VECTOR2_ARRAY:
-			return "PoolVector2Array"
-		TYPE_VECTOR3_ARRAY:
-			return "PoolVector3Array"
-		TYPE_COLOR_ARRAY:
-			return "PoolColorArray"
-		_:
-			printerr("Type value %s should be a member of the TYPE_* enum, but it is not.")
-	return "[ERROR, nonexistent type value %s]" % type
+func convert_type_index_to_text(type: int) -> String:
+	if type in TYPE_MAP:
+		return TYPE_MAP[type]
+	else:
+		printerr("Type value %s should be a member of the TYPE_* enum, but it is not.")
+		return "[ERROR, nonexistent type value %s]" % type
