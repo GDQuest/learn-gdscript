30a8fc09860ecf5cf92eaf462143b52df3f2e8b3
Henrique Lacreta Alves <henriquelalves@gmail.com>
2022-10-09 06:23:59 -0300

    fix: restore Traffic Light example in Lesson 15 (#696)
    Fix #679

--- course/lesson-15-modulo/visuals/TrafficLightsModulo.gd
+++ course/lesson-15-modulo/visuals/TrafficLightsModulo.gd
@@ -2,8 +2,7 @@ extends Node2D
 
 signal line_highlight_requested
 
-var initial_light_index := 0
-var light_index := initial_light_index - 1
+var light_index := 0
 
 onready var _tween := $Tween as Tween
 onready var _index_label := $Index as Label
@@ -19,35 +18,37 @@ func _ready() -> void:
 
 
 func run() -> void:
-	emit_signal("line_highlight_requested", 0)
-	yield()
-	
-	if _tween.is_active():
-		return
-	
-	light_index += 1
-	_index_label.text = str(light_index)
-	emit_signal("line_highlight_requested", 1)
-	yield()
-	light_index %= 3
-	_index_label.text = str(light_index)
 	emit_signal("line_highlight_requested", 2)
 	yield()
 	
-	turn_on_light(light_index)
+	light_index += 1
+	_index_label.text = str(light_index)
+	emit_signal("line_highlight_requested", 3)
+	yield()
+	light_index %= 3
+	_index_label.text = str(light_index)
+	emit_signal("line_highlight_requested", 4)
+	yield()
+	
+	turn_on_light(light_index, false)
+	emit_signal("line_highlight_requested", 5)
+	yield()
 
 
 func reset() -> void:
-	light_index = initial_light_index
-	turn_on_light(initial_light_index)
-	
+	turn_on_light(light_index, true)
 	_index_label.text = str(light_index)
 
 
-func turn_on_light(_light_index: int) -> void:
+func turn_on_light(_light_index: int, skip: bool) -> void:
+	if _tween.is_active():
+		_tween.seek(1.0)
 	
 	for light in _lights:
 		_tween.interpolate_property(light, "modulate:a", light.modulate.a, 0, 0.2, Tween.TRANS_EXPO, Tween.EASE_OUT)
 	
 	_tween.interpolate_property(_lights[light_index], "modulate:a", 0, 1, 0.2, Tween.TRANS_EXPO, Tween.EASE_OUT, 0.2)
 	_tween.start()
+	
+	if skip:
+		_tween.seek(1.0)
