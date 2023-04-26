0517b5e4c12e5b94207d9552f79dae7bd2ced005
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2023-04-26 18:35:27 +0200

    refactor rest of the code

--- i18n/check_and_integrate_translations.py
+++ i18n/check_and_integrate_translations.py
@@ -3,10 +3,39 @@ import sys
 import argparse
 import subprocess
 import shutil
+import glob
 from match_and_merge_po_translations import parse_po_file
+from dataclasses import dataclass
 
 
-def parse_command_line_arguments():
+@dataclass
+class TranslationsData:
+    language_code: str
+    directory_path: str
+    total_strings: int = 0
+    missing_translations: int = 0
+    fuzzy_translations: int = 0
+
+    _completion_rate: int = -1
+
+    def get_completion_rate(self) -> int:
+        """Returns the completion rate of the language with an int between 0 and 100, with 100 representing 100% completion."""
+        if self._completion_rate == -1:
+            todo_count = self.missing_translations + self.fuzzy_translations
+            self._completion_rate = 100 - (100 * todo_count // self.total_strings)
+
+        return self._completion_rate
+
+
+@dataclass
+class Args:
+    translations_path: str
+    threshold: int
+    skip_extract: bool
+    skip_sync: bool
+
+
+def parse_command_line_arguments() -> Args:
     """
     Parses command line arguments and returns a dictionary containing options and arguments.
     """
@@ -39,16 +68,17 @@ def parse_command_line_arguments():
         action="store_true",
         help="Skip the synchronization and merge of PO files with the reference POT files.",
     )
-    return vars(parser.parse_args())
+    return Args(**vars(parser.parse_args()))
 
 
 def main():
     args = parse_command_line_arguments()
 
     I18N_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
-    SOURCE_DIRECTORY = os.path.join(I18N_DIRECTORY, "..")
 
-    if not args["skip_extract"]:
+    if args.skip_extract:
+        print("WARN: Skipping strings extraction and POT files generation...")
+    else:
         print(
             "INFO: Extracting translation strings from learn-gdscript source code and generating POT files..."
         )
@@ -65,93 +95,83 @@ def main():
             )
 
         print("INFO: Moving POT files to translations folder")
-        pot_files = [
-            file for file in os.listdir(I18N_DIRECTORY) if file.endswith(".pot")
-        ]
+
+        pot_files = glob.glob("*.pot", root_dir=I18N_DIRECTORY)
         for pot_file in pot_files:
             shutil.move(
                 os.path.join(I18N_DIRECTORY, pot_file),
-                os.path.join(args["translations_path"], pot_file),
+                os.path.join(args.translations_path, pot_file),
             )
-    else:
-        print("WARN: Skipping strings extraction and POT files generation...")
 
     # Updating PO files with sync_translations.py
-    # TODO: remove chdir, update from here down
-    os.chdir(args["translations_path"])
-    source_dir = os.path.join(args["translations_path"], "source")
-    if not args["skip_sync"]:
-        print("INFO: Running synchronization script")
-        subprocess.run(["python3", "sync_translations.py"])
-    else:
+    sync_translations_script_path = os.path.join(
+        args.translations_path, "sync_translations.py"
+    )
+    if args.skip_sync:
         print("WARN: Skipping PO files merging with POT...")
+    else:
+        print("INFO: Running synchronization script")
+        subprocess.run(["python3", sync_translations_script_path])
 
     # Parsing and Analyzing PO files
     print("INFO: Parsing PO files and counting missing translations")
-    languages_directories = [
-        lan_dir.path
-        for lan_dir in os.scandir()
-        if lan_dir.is_dir() and lan_dir.path[:3] != "./."
-    ]
-    analysis_results = {}
-    for lan_dir in languages_directories:
-        po_files = [
-            os.path.join(lan_dir, file)
-            for file in os.listdir(lan_dir)
-            if file.endswith(".po")
-        ]
+    languages_directories = []
+    for name in os.listdir(args.translations_path):
+        folder_path = os.path.join(args.translations_path, name)
+        if os.path.isdir(folder_path):
+            languages_directories.append(name)
+
+    translation_datas = []
+    for language_directory in languages_directories:
+        po_files = glob.glob("*.po", root_dir=language_directory)
         parsed_po_files = list(map(parse_po_file, po_files))
+        data = TranslationsData(
+            language_code=os.path.basename(language_directory),
+            directory_path=language_directory,
+        )
 
         # Iterating through PO files entries in order to count missing and fuzzy translations
-        missing_translations = 0
-        fuzzy_translations = 0
-        strings_number = 0
         for po_file in parsed_po_files:
-            strings_number += len(po_file.entries)
+            data.total_strings = len(po_file.entries)
             for entry in po_file.entries:
                 if entry.msgstr == "" and entry.msgid != "":
                     # Case 1 : entry has no translated string whereas it has an id value
-                    missing_translations += 1
+                    data.missing_translations += 1
                 elif entry.is_fuzzy:
                     # Case 2 : entry has a translation, but it is tagged as fuzzy by msgmerge
-                    fuzzy_translations += 1
+                    data.fuzzy_translations += 1
 
-        if strings_number > 0:
-            analysis_results[lan_dir] = {
-                "total": strings_number,
-                "missings": missing_translations,
-                "fuzzies": fuzzy_translations,
-            }
+        if data.total_strings > 0:
+            translation_datas.append(data)
 
     # Computing translations indicator values
     print("INFO: Computing completion indicator for each language.")
-    languages_to_integrate = list()
-    threshold = args["threshold"]
-    for lang, results in analysis_results.items():
-        results["completion"] = 100 - (100 * results["missings"] // results["total"])
-        results["rework"] = (
-            100 * results["fuzzies"] // (results["total"] - results["missings"])
-        )
-        if threshold is not None and results["completion"] >= threshold:
-            languages_to_integrate.append(lang)
 
     # Sorting and Outputting results
-    for lang, results in sorted(
-        analysis_results.items(), key=lambda v: v[1]["completion"], reverse=True
+    for data in sorted(
+        translation_datas, key=lambda data: data.get_completion_rate(), reverse=True
     ):
         print(
-            f"Language : {lang.upper()[2:]} - {results['completion']}% including {results['rework']}% fuzzy"
+            f"Language : {data.language_code} - {data.get_completion_rate()}% "
+            f"including {data.fuzzy_translations}% fuzzy and {data.missing_translations}% missing translations."
         )
 
+    languages_to_integrate = [
+        data
+        for data in translation_datas
+        if data.get_completion_rate() >= args.threshold
+    ]
     # Integrating translations in GD_Learn project
-    if len(languages_to_integrate) > 0:
-        print(f"INFO: Integrating languages above {threshold}%.")
-        for lang in languages_to_integrate:
-            print("Copying ", lang)
-            dest = os.path.join(source_dir, "i18n", lang)
-            if os.path.exists(dest):
-                shutil.rmtree(dest)
-            shutil.copytree(lang, dest)
+    if languages_to_integrate:
+        print(
+            f"INFO: Integrating languages with translations above {args.threshold}% complete."
+        )
+        for data in languages_to_integrate:
+            print("Copying ", data.language_code)
+            destination = os.path.join(I18N_DIRECTORY, data.language_code)
+            if os.path.exists(destination):
+                shutil.rmtree(destination)
+            shutil.copytree(data.directory_path, destination)
     else:
         print("WARN: No language complete enough to be integrated.")
 
