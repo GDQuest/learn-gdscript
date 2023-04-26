26250ef9db2e4293f30652f513dfcb7e4ab1c450
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2023-04-26 14:20:41 +0200

    run formatter

--- i18n/check_and_integrate_translations.py
+++ i18n/check_and_integrate_translations.py
@@ -10,14 +10,35 @@ def parse_command_line_arguments():
     """
     Parses command line arguments and returns a dictionary containing options and arguments.
     """
-    parser = argparse.ArgumentParser(description="This script performs text extraction from the application and move "
-                                                 "generated POT files in the translations project in order to compare "
-                                                 "with translations source and output a translation completion "
-                                                 "indicator for each language.")
-    parser.add_argument("translations_path", help="Relative or absolute path to the repository learn-gdscript-translations/.")
-    parser.add_argument("-t", "--threshold", type=int, default=95, help="Minimum completion percentage value for a language to be integrated.")
-    parser.add_argument("-E", "--skip-extract", action="store_true", help="Skip the extraction of strings and POT files generation.")
-    parser.add_argument("-S", "--skip-sync", action="store_true", help="Skip the synchronization and merge of PO files with the reference POT files.")
+    parser = argparse.ArgumentParser(
+        description="This script performs text extraction from the application and move "
+        "generated POT files in the translations project in order to compare "
+        "with translations source and output a translation completion "
+        "indicator for each language."
+    )
+    parser.add_argument(
+        "translations_path",
+        help="Relative or absolute path to the repository learn-gdscript-translations/.",
+    )
+    parser.add_argument(
+        "-t",
+        "--threshold",
+        type=int,
+        default=95,
+        help="Minimum completion percentage value for a language to be integrated.",
+    )
+    parser.add_argument(
+        "-E",
+        "--skip-extract",
+        action="store_true",
+        help="Skip the extraction of strings and POT files generation.",
+    )
+    parser.add_argument(
+        "-S",
+        "--skip-sync",
+        action="store_true",
+        help="Skip the synchronization and merge of PO files with the reference POT files.",
+    )
     return vars(parser.parse_args())
 
 
@@ -27,22 +48,31 @@ def main():
     I18N_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
     SOURCE_DIRECTORY = os.path.join(I18N_DIRECTORY, "..")
 
-    if not args['skip_extract']:
-        print("INFO: Extracting translation strings from learn-gdscript source code and generating POT files...")
+    if not args["skip_extract"]:
+        print(
+            "INFO: Extracting translation strings from learn-gdscript source code and generating POT files..."
+        )
         EXTRACT_SCRIPT_PATH = os.path.join(I18N_DIRECTORY, "extract.py")
-        result = subprocess.run([ "python3", EXTRACT_SCRIPT_PATH], capture_output=True)
+        result = subprocess.run(["python3", EXTRACT_SCRIPT_PATH], capture_output=True)
 
         # if there was an error, print stdout and stderr and exit
         if result.returncode > 0:
-            sys.exit("ERROR: Extraction scripts ended with errors. Aborting script.\n" 
-                     f"Script {EXTRACT_SCRIPT_PATH} output the following errors:"
-                     f"Error code: {result.returncode}"
-                     f"\n{result.stderr.decode('utf-8')}")
+            sys.exit(
+                "ERROR: Extraction scripts ended with errors. Aborting script.\n"
+                f"Script {EXTRACT_SCRIPT_PATH} output the following errors:"
+                f"Error code: {result.returncode}"
+                f"\n{result.stderr.decode('utf-8')}"
+            )
 
         print("INFO: Moving POT files to translations folder")
-        pot_files = [file for file in os.listdir(I18N_DIRECTORY) if file.endswith(".pot")]
+        pot_files = [
+            file for file in os.listdir(I18N_DIRECTORY) if file.endswith(".pot")
+        ]
         for pot_file in pot_files:
-            shutil.move(os.path.join(I18N_DIRECTORY, pot_file), os.path.join(args["translations_path"], pot_file))
+            shutil.move(
+                os.path.join(I18N_DIRECTORY, pot_file),
+                os.path.join(args["translations_path"], pot_file),
+            )
     else:
         print("WARN: Skipping strings extraction and POT files generation...")
 
@@ -58,10 +88,18 @@ def main():
 
     # Parsing and Analyzing PO files
     print("INFO: Parsing PO files and counting missing translations")
-    languages_directories = [lan_dir.path for lan_dir in os.scandir() if lan_dir.is_dir() and lan_dir.path[:3] != './.']
+    languages_directories = [
+        lan_dir.path
+        for lan_dir in os.scandir()
+        if lan_dir.is_dir() and lan_dir.path[:3] != "./."
+    ]
     analysis_results = {}
     for lan_dir in languages_directories:
-        po_files = [os.path.join(lan_dir, file) for file in os.listdir(lan_dir) if file.endswith(".po")]
+        po_files = [
+            os.path.join(lan_dir, file)
+            for file in os.listdir(lan_dir)
+            if file.endswith(".po")
+        ]
         parsed_po_files = list(map(parse_po_file, po_files))
 
         # Iterating through PO files entries in order to count missing and fuzzy translations
@@ -71,7 +109,7 @@ def main():
         for po_file in parsed_po_files:
             strings_number += len(po_file.entries)
             for entry in po_file.entries:
-                if entry.msgstr == '' and entry.msgid != '':
+                if entry.msgstr == "" and entry.msgid != "":
                     # Case 1 : entry has no translated string whereas it has an id value
                     missing_translations += 1
                 elif entry.is_fuzzy:
@@ -79,9 +117,11 @@ def main():
                     fuzzy_translations += 1
 
         if strings_number > 0:
-            analysis_results[lan_dir] = {"total": strings_number,
-                                         "missings": missing_translations,
-                                         "fuzzies": fuzzy_translations}
+            analysis_results[lan_dir] = {
+                "total": strings_number,
+                "missings": missing_translations,
+                "fuzzies": fuzzy_translations,
+            }
 
     # Computing translations indicator values
     print("INFO: Computing completion indicator for each language.")
@@ -89,13 +129,19 @@ def main():
     threshold = args["threshold"]
     for lang, results in analysis_results.items():
         results["completion"] = 100 - (100 * results["missings"] // results["total"])
-        results["rework"] = 100 * results['fuzzies'] // (results['total'] - results["missings"])
+        results["rework"] = (
+            100 * results["fuzzies"] // (results["total"] - results["missings"])
+        )
         if threshold is not None and results["completion"] >= threshold:
             languages_to_integrate.append(lang)
 
     # Sorting and Outputting results
-    for lang, results in sorted(analysis_results.items(), key=lambda v: v[1]["completion"], reverse=True):
-        print(f"Language : {lang.upper()[2:]} - {results['completion']}% including {results['rework']}% fuzzy")
+    for lang, results in sorted(
+        analysis_results.items(), key=lambda v: v[1]["completion"], reverse=True
+    ):
+        print(
+            f"Language : {lang.upper()[2:]} - {results['completion']}% including {results['rework']}% fuzzy"
+        )
 
     # Integrating translations in GD_Learn project
     if len(languages_to_integrate) > 0:
