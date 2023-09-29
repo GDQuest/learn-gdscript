f7a8940f418a8d0c6c869e3a7553210ef08ad619
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2023-09-29 12:59:45 +0200

    content: remove type hint from code in l1 practice

--- course/lesson-1-what-code-is-like/first_practice/WelcomeToTheApp.gd
+++ course/lesson-1-what-code-is-like/first_practice/WelcomeToTheApp.gd
@@ -3,7 +3,7 @@ extends Node2D
 onready var _animation_tree := find_node("AnimationTree")
 
 # EXPORT welcome_to_app
-func _ready() -> void:
+func _ready():
 	print("Welcome!")
 # /EXPORT welcome_to_app
 	yield(get_tree().create_timer(1.0), "timeout")
