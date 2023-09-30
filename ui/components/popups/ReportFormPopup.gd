fab780e81a452911e6ca36abe307694c03a9a385
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2023-09-30 09:04:43 +0200

    fix: apply translations to report problem popup
    fix #836

--- ui/components/popups/ReportFormPopup.gd
+++ ui/components/popups/ReportFormPopup.gd
@@ -5,13 +5,17 @@ onready var _panel := $PanelContainer as PanelContainer
 
 onready var _confirm_button := $PanelContainer/Column/Margin/Column/ConfirmButton as Button
 onready var _summary_label := $PanelContainer/Column/Margin/Column/Summary as RichTextLabel
+onready var _title_label: Label = $PanelContainer/Column/Margin/Column/Title
+
+onready var _title := _title_label.text
+onready var _summary := _summary_label.bbcode_text
 
 
 func _ready():
 	hide()
 	_confirm_button.connect("pressed", self, "hide")
 	_summary_label.connect("meta_clicked", self, "_on_meta_clicked")
-
+	_update_translations()
 
 func show() -> void:
 	_color_rect.show()
@@ -30,3 +34,14 @@ func _on_meta_clicked(data) -> void:
 			OS.shell_open(data)
 		elif data == "download":
 			Log.download()
+
+
+func _notification(what: int) -> void:
+	if what == NOTIFICATION_TRANSLATION_CHANGED:
+		_update_translations()
+
+
+func _update_translations() -> void:
+	if _title_label:
+		_title_label.text = tr(_title)
+		_summary_label.bbcode_text = tr(_summary)
