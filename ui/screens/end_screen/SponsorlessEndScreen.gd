a3ed1172a0d1c3fe2b5ff7f901adf461877ad09b
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2024-12-03 15:10:32 +0100

    fix: ensure official docs end screen links are clickable
    fix #1021

--- ui/screens/end_screen/SponsorlessEndScreen.gd
+++ ui/screens/end_screen/SponsorlessEndScreen.gd
@@ -1,12 +1,22 @@
 extends Control
 
 onready var _outliner_button := $Layout/TopBar/MarginContainer/ToolBarLayout/OutlinerButton
-
+onready var _rich_text_labels := [
+	$Layout/PanelContainer/Sky/Control/Panel/Margin/ScrollContainer/Column/RichTextLabel,
+	$Layout/PanelContainer/Sky/Control/Panel/Margin/ScrollContainer/Column/RichTextLabel2,
+]
 
 func _ready() -> void:
 	_outliner_button.connect("pressed", self, "_on_outliner_button_pressed")
+	for rtl in _rich_text_labels:
+		rtl.connect("meta_clicked", self, "_on_meta_clicked")
 
 
 func _on_outliner_button_pressed() -> void:
 	NavigationManager.emit_signal("outliner_navigation_requested")
 	hide()
+
+
+func _on_meta_clicked(data) -> void:
+	if typeof(data) == TYPE_STRING:
+		OS.shell_open(data)
