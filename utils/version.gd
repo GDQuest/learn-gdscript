9002866c3f2daac7b6f999f5ecea2515f13ca73e
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2024-12-12 11:39:39 +0100

    chore: remove auto-generated version script

--- utils/version.gd
+++
@@ -1,9 +0,0 @@
-# AUTO GENERATED changed_file, YOUR CHANGES WILL NOT REMAIN
-class_name AppVersion
-
-const version := "1.5.2";
-const git_commit := "7145280c52cf71d445f8b87bc13a66b1a10a845b";
-const git_branch := "nathan/build-script-modified";
-const build_date := [2023, 11, 07, 15, 55, 59];
-const build_date_unix := 1699368959;
-const build_date_iso := "2023/11/07 15:55:59;"
