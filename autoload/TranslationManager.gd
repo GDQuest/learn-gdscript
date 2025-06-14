05290804509718ea78aab970be966b79fe2a6735
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2025-06-14 15:20:08 +0200

    translations: Update translation files and integrate several new languages

--- autoload/TranslationManager.gd
+++ autoload/TranslationManager.gd
@@ -18,6 +18,9 @@ const SUPPORTED_LOCALES := [
 	"de",
 	"tr",
 	"nl",
+	"uk",
+	"zh_Hant",
+	"cs",
 ]
 
 const LOCALE_TO_LABEL := {
@@ -31,6 +34,9 @@ const LOCALE_TO_LABEL := {
 	"de": "Deutsch",
 	"tr": "Türkçe",
 	"nl": "Nederlands",
+	"uk": "Українська",
+	"zh_Hant": "繁體中文",
+	"cs": "Čeština",
 }
 
 
@@ -46,34 +52,34 @@ func _ready() -> void:
 
 func get_available_languages() -> Array:
 	var languages := []
-	
+
 	for locale_code in SUPPORTED_LOCALES:
 		var language_name: String = LOCALE_TO_LABEL.get(locale_code, "")
 		if language_name == "":
 			language_name = TranslationServer.get_locale_name(locale_code)
-			
+
 		languages.append({
 			"code": locale_code,
 			"name": language_name,
 		})
-	
+
 	return languages
 
 
 func set_language(language_code: String) -> void:
 	if current_language == language_code:
 		return
-	
+
 	current_language = language_code
 	var current_profile := UserProfiles.get_profile()
-	
+
 	# Remove existing translations from the translation server.
 	if _loaded_translations.size() > 0:
 		for translation in _loaded_translations:
 			TranslationServer.remove_translation(translation)
-		
+
 		_loaded_translations = []
-	
+
 	# If the language is set to the default locale, we don't need to do anything else.
 	if current_language == DEFAULT_LOCALE:
 		TranslationServer.set_locale(current_language)
@@ -81,55 +87,55 @@ func set_language(language_code: String) -> void:
 		current_profile.save()
 		emit_signal("translation_changed")
 		return
-	
+
 	# Load order shouldn't be important, so we'll just load everything from the folder.
 	var locale_dir_path := I18N_ROOT.plus_file(current_language)
-	
+
 	var fs := Directory.new()
 	if not fs.dir_exists(locale_dir_path):
 		printerr("Failed to change language to '%s': Language folder does not exist." % [ current_language ])
 		_reset_language()
 		return
-	
+
 	var error = fs.change_dir(locale_dir_path)
 	if error:
 		printerr("Failed to open language folder for '%s': Error code %d" % [ current_language, error ])
 		_reset_language()
 		return
-	
+
 	error = fs.list_dir_begin(true, true)
 	if error:
 		printerr("Failed to list language folder for '%s': Error code %d" % [ current_language, error ])
 		_reset_language()
 		return
-	
+
 	# Iterate through all PO files and try to load them.
 	var file_path = fs.get_next()
 	while file_path:
 		if not file_path.get_extension() == PO_EXTENSION:
 			file_path = fs.get_next()
 			continue
-		
+
 		var full_path = locale_dir_path.plus_file(file_path)
-		
+
 		if not ResourceLoader.exists(full_path):
 			printerr("Language file at '%s' is not recognized as a valid resource." % [ full_path ])
 			file_path = fs.get_next()
 			continue
-		
+
 		var translation := ResourceLoader.load(full_path, "Translation") as Translation
 		if not translation:
 			printerr("Language resource at '%s' has failed to load." % [ full_path ])
 			file_path = fs.get_next()
 			continue
-		
+
 		_loaded_translations.append(translation)
 		file_path = fs.get_next()
-	
+
 	# Add loaded translations to the translation server.
 	for translation in _loaded_translations:
 		TranslationServer.add_translation(translation)
-	
+
 	# Set the language to update the app.
 	TranslationServer.set_locale(current_language)
 	current_profile.language = current_language
@@ -140,7 +146,7 @@ func set_language(language_code: String) -> void:
 func _reset_language() -> void:
 	current_language = DEFAULT_LOCALE
 	TranslationServer.set_locale(current_language)
-	
+
 	var current_profile := UserProfiles.get_profile()
 	current_profile.language = current_language
 	current_profile.save()
