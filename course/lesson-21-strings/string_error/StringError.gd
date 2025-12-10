d68686c488c048ffc2cf6ba3d035992a99100f02
moss <59064958+mstphayassin@users.noreply.github.com>
2025-12-10 05:58:52 -0700

    Fix: correct logic for tests in 20.1 and provide more descriptive errors (#1170)
    * corrected logic for tests in 20.1 and provide more descriptive errors
    
    * c
    
    ---------
    
    Co-authored-by: Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>

--- course/lesson-21-strings/string_error/StringError.gd
+++ course/lesson-21-strings/string_error/StringError.gd
@@ -12,14 +12,16 @@ func _run() -> void:
 	run()
 	if robot_name is String:
 		_label.text = robot_name
+	else:
+		_label.text = "robot_name"
 	yield(get_tree().create_timer(1.0), "timeout")
 	_animation_tree.travel("saying_hi")
 	Events.emit_signal("practice_run_completed")
 
 
-var robot_name = -1
-
 # EXPORT print_string
+var robot_name = "Robi"
+
 func run():
-	robot_name = "Robi"
+	print("Hi, " + robot_name + "!")
 # /EXPORT print_string
--- course/lesson-21-strings/string_error/TestsStringError.gd
+++ course/lesson-21-strings/string_error/TestsStringError.gd
@@ -3,6 +3,7 @@ extends PracticeTester
 var robot: Node2D
 var has_name_prop := false
 
+
 func _prepare() -> void:
 	robot = _scene_root_viewport.get_child(0)
 	has_name_prop = false
@@ -12,16 +13,13 @@ func _prepare() -> void:
 			break
 
 
-func test_has_robot_name_variable() -> String:
-	if not has_name_prop:
-		return "The robot_name variable doesn't exist. Did you define it with the var keyword?"
-	return ""
-
-
 func test_robot_name_is_a_string() -> String:
 	if not has_name_prop:
-		return "The robot_name variable doesn't exist. Make sure it's defined."
-	var robot_name = robot.get("robot_name")
-	if not robot_name is String:
-		return "The robot_name isn't a string. Did you add quotes around the value?"
+		return tr("The robot_name variable doesn't exist.")
+
+	var value = robot.get("robot_name")
+	if value is int and value == 0:
+		return tr("The robot_name variable is set to 0. Did you change its value?")
+	elif not value is String:
+		return tr("The robot_name isn't a string. Did you add quotes around the value?")
 	return ""
