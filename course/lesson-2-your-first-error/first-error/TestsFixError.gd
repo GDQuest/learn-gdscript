1cece70538c5f4d28c281aeff9c1da898c992d50
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2025-12-11 10:43:36 +0100

    Fix lesson 2 practice not passing anymore

--- course/lesson-2-your-first-error/first-error/TestsFixError.gd
+++ course/lesson-2-your-first-error/first-error/TestsFixError.gd
@@ -1,8 +1,7 @@
 extends PracticeTester
 
-
 func test_remove_errors() -> String:
-	for line in _slice.get_main_lines():
+	for line in _slice.get_slice_text():
 		if "#" in line:
 			return tr("There's still a comment in your code. You need to remove the comment sign (#).")
 	return ""
