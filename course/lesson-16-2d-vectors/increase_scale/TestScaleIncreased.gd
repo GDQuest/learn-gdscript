3adfd4f8c255686feb0cbf4ea26f278684490951
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2026-07-25 21:55:15 +0200

    Fix L15.P1 not passing with new AST check

--- course/lesson-16-2d-vectors/increase_scale/TestScaleIncreased.gd
+++ course/lesson-16-2d-vectors/increase_scale/TestScaleIncreased.gd
@@ -14,26 +14,29 @@ func _define(checks: Array[Check]) -> void:
 
 func test_use_a_vector_to_increase_scale() -> String:
 	var level_up_function := _analyzer.get_function_named("level_up")
-	
+	# FIXME: Patching with a regex check for release as AST check doesn't work
+	if matches_code_line_regex([r"^\s*scale\s*\+=\s*Vector2\s*\(\s*0\.2\s*,\s*0\.2\s*\)"]):
+		return ""
+
 	if not GDExpr.suite(
 		GDExpr.any_of(
 			GDExpr.assignment(
 				GDExpr.identifier("scale"),
 				GDExpr.literal(Vector2(0.2, 0.2), true),
-				GDAssignmentNode.OP_MULTIPLICATION
+				GDAssignmentNode.OP_ADDITION
 			),
 			GDExpr.assignment(
 				GDExpr.identifier("scale"),
 				GDExpr.bin_op(
 					GDExpr.identifier("scale"),
 					GDExpr.literal(Vector2(0.2, 0.2), true),
-					GDBinaryOpNode.OP_MULTIPLICATION
+					GDBinaryOpNode.OP_ADDITION
 				)
 			)
 		)
 	).matches(level_up_function):
 		return tr("It looks like the scale isn't increasing by some vector. Did you add a vector to scale?")
-	
+
 	return ""
 
 
@@ -43,4 +46,3 @@ func test_correct_scale_after_2_levels() -> String:
 		return ""
 
 	return tr("scale's value is %s; It should be (1.4, 1.4) after levelling up 2 times.") % scale
-
