afa0fbb9e29b7c1c2970626f3bc7fb2520dfddfd
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2026-07-26 10:27:20 +0200

    L10.P1: Fix negative rotations passing the practice successfully
    Fix #1313

--- course/lesson-10-the-game-loop/rotating-sprite/TestsRotating.gd
+++ course/lesson-10-the-game-loop/rotating-sprite/TestsRotating.gd
@@ -1,30 +1,34 @@
 extends PracticeTester
 
 var robot: Node2D
-var lines: PackedStringArray
 
 
 func _prepare() -> void:
 	robot = _scene_root_viewport.get_child(0).get_node("Robot")
-	for line in _slice.current_text.split("\n"):
-		line = line.strip_edges().replace(" ", "")
-		if not line.is_empty():
-			lines.push_back(line)
 
 func _define(checks: Array[Check]) -> void:
 	checks.append(Check.new(tr("Character Is Rotating Clockwise"), tr(""), test_character_is_rotating_clockwise))
 
 
 func test_character_is_rotating_clockwise() -> String:
-	var has_rotate := false
-	for line in lines:
-		if line.begins_with("rotate("):
-			has_rotate = true
-			break
+	var process := _analyzer.get_function_named("_process")
 
-	if not has_rotate:
+	if not process or not GDExpr.suite(GDExpr.function_call("rotate", null)).matches(process):
 		return tr("Did you use rotate() to make the sprite rotate?")
+
+	for statement in process.get_body().get_statements():
+		if statement.get_type() != GDNode.CALL:
+			continue
+		var call := statement as GDCallNode
+		if call.get_function_name() != "rotate" or call.get_arguments().is_empty():
+			continue
+		var argument := call.get_arguments()[0] as GDLiteralNode
+		if not argument:
+			continue
+		var value: Variant = argument.get_reduced_value()
+		if (value is int or value is float) and value < 0.0:
+			return tr("The robot is turning in the wrong direction!")
+
 	if robot.rotation < 0.0:
 		return tr("The robot is turning in the wrong direction!")
 	return ""
-
